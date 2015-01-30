2be4f0931c3b00e60d111dd00c2c88e1f5c3833a
Rohan Dhamal <rohan.dhamal@claricetechnologies.com>
2015-01-30 18:33:05 +0530

    Use manifest versionName and environment display name to show in the app

--- android/source/VideoLocker/src/org/edx/mobile/util/PropertyUtil.java
+++ android/source/VideoLocker/src/org/edx/mobile/util/PropertyUtil.java
@@ -1,6 +1,9 @@
 package org.edx.mobile.util;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
 import java.io.InputStream;
 import java.util.Properties;
 import org.edx.mobile.logger.Logger;
@@ -9,25 +12,11 @@ public class PropertyUtil {
 
     private static final Logger logger = new Logger(PropertyUtil.class.getName());
 
-    public static String getDisplayVersionName(Context context) {
+    public static String getManifestVersionName(Context context) {
         try {
-            Properties props=new Properties();
-            InputStream inputStream = context.getAssets().open("config/config.properties");
-            props.load(inputStream);
-            
-            String versionName = props.getProperty("versionName");
-            String versionNameSuffix = props.getProperty("versionNameSuffix");
-            String buildConfig = props.getProperty("buildConfig");
-            if (buildConfig != null && buildConfig.equalsIgnoreCase("prod")) {
-                buildConfig = "";
-            }
-            
-            if (buildConfig != null && buildConfig.length() > 1) {
-                // init capital for the config name
-                buildConfig = Character.toUpperCase(buildConfig.charAt(0)) + buildConfig.substring(1);
-            }
-            
-            return versionName + "." + versionNameSuffix + " " + buildConfig;
+            PackageInfo pInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            return pInfo.versionName;
         } catch(Exception ex) {
             logger.error(ex);
         }
