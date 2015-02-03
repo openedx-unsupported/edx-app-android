12c681bcd6fd31c71b78adc16c32d4eb2f77ff89
shahidtamboli <shahid.tamboli@claricetechnologies.com>
2015-02-03 18:56:49 +0530

    offline mode messages

--- android/source/VideoLocker/src/org/edx/mobile/view/FindCoursesActivity.java
+++ android/source/VideoLocker/src/org/edx/mobile/view/FindCoursesActivity.java
@@ -3,12 +3,17 @@ package org.edx.mobile.view;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
 import org.edx.mobile.R;
 import org.edx.mobile.base.BaseFragmentActivity;
 import org.edx.mobile.util.AppConstants;
 import org.edx.mobile.util.NetworkUtil;
+import org.edx.mobile.view.custom.ETextView;
 
 public class FindCoursesActivity extends BaseFragmentActivity {
+    private View offlineBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,29 @@ public class FindCoursesActivity extends BaseFragmentActivity {
         }catch(Exception e){
             logger.error(e);
         }
+
+        offlineBar = findViewById(R.id.offline_bar);
+        if (!(NetworkUtil.isConnected(this))) {
+            AppConstants.offline_flag = true;
+            invalidateOptionsMenu();
+            showOfflineMessage();
+            offlineBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onOnline() {
+        offlineBar.setVisibility(View.GONE);
+        hideOfflineMessage();
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    protected void onOffline() {
+        offlineBar.setVisibility(View.VISIBLE);
+        showOfflineMessage();
+        hideLoadingProgress();
+        invalidateOptionsMenu();
     }
 
     @Override
@@ -36,8 +64,62 @@ public class FindCoursesActivity extends BaseFragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
 
+        //TODO - Remove this after the PR for removing checkbox is merged with master
         MenuItem checkBox_menuItem = menu.findItem(R.id.delete_checkbox);
         checkBox_menuItem.setVisible(false);
+
+        MenuItem menuItem = menu.findItem(R.id.progress_download);
+        menuItem.setVisible(false);
+
         return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.findItem(R.id.progress_download);
+        menuItem.setVisible(false);
+        return true;
+    }
+
+    /**
+     * This function shows the offline mode message
+     */
+    private void showOfflineMessage(){
+        ETextView offlineModeTv = (ETextView) findViewById(R.id.offline_mode_message);
+        if(offlineModeTv!=null){
+            offlineModeTv.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * This function hides the offline mode message
+     */
+    private void hideOfflineMessage(){
+        ETextView offlineModeTv = (ETextView) findViewById(R.id.offline_mode_message);
+        if(offlineModeTv!=null){
+            offlineModeTv.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * This function shows the loading progress wheel
+     * Show progress wheel while loading the web page
+     */
+    private void showLoadingProgress(){
+        ProgressBar progressWheel = (ProgressBar) findViewById(R.id.progress_spinner);
+        if(progressWheel!=null){
+            progressWheel.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * This function hides the loading progress wheel
+     * Hide progress wheel after the web page completes loading
+     */
+    private void hideLoadingProgress(){
+        ProgressBar progressWheel = (ProgressBar) findViewById(R.id.progress_spinner);
+        if(progressWheel!=null){
+            progressWheel.setVisibility(View.GONE);
+        }
     }
 }
