a4e0d35aa580320c323bd1805df008f73487226c
Rohan Dhamal <rohan.dhamal@claricetechnologies.com>
2015-02-12 01:31:52 +0530

    Fix database upgrade queries

--- android/source/VideoLocker/src/org/edx/mobile/module/db/impl/DbHelper.java
+++ android/source/VideoLocker/src/org/edx/mobile/module/db/impl/DbHelper.java
@@ -1,11 +1,11 @@
 package org.edx.mobile.module.db.impl;
 
-import org.edx.mobile.logger.Logger;
-import org.edx.mobile.module.db.DbStructure;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import org.edx.mobile.logger.Logger;
+import org.edx.mobile.module.db.DbStructure;
 
 /**
  * This class is an implementation of {@link SQLiteOpenHelper} and handles
@@ -55,16 +55,36 @@ class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        try{
-            String upgradeQuery = "ALTER TABLE "    + DbStructure.Table.DOWNLOADS
-                    + " ADD COLUMN " + DbStructure.Column.UNIT_URL + " TEXT "
-                    + " ADD COLUMN " + DbStructure.Column.URL_HIGH_QUALITY + " TEXT "
-                    + " ADD COLUMN " + DbStructure.Column.URL_LOW_QUALITY + " TEXT "
-                    + " ADD COLUMN " + DbStructure.Column.URL_YOUTUBE + " TEXT ";
+        try {
+            String upgradeToV2 =
+                    "ALTER TABLE "    + DbStructure.Table.DOWNLOADS + " ADD COLUMN "
+                                      + DbStructure.Column.UNIT_URL + " TEXT ";
+
+            String[] upgradeToV3 = new String[] {
+                    "ALTER TABLE "    + DbStructure.Table.DOWNLOADS + " ADD COLUMN "
+                                      + DbStructure.Column.UNIT_URL + " TEXT ",
+
+                    "ALTER TABLE "    + DbStructure.Table.DOWNLOADS + " ADD COLUMN "
+                                      + DbStructure.Column.URL_HIGH_QUALITY + " TEXT ",
+
+                    "ALTER TABLE "    + DbStructure.Table.DOWNLOADS + " ADD COLUMN "
+                                      + DbStructure.Column.URL_LOW_QUALITY + " TEXT ",
+
+                    "ALTER TABLE "    + DbStructure.Table.DOWNLOADS + " ADD COLUMN "
+                                      + DbStructure.Column.URL_YOUTUBE + " TEXT "};
+
             if (oldVersion == 1 && newVersion == 2) {
-                db.execSQL(upgradeQuery);
+                // upgrade from 1 to 2
+                db.execSQL(upgradeToV2);
             }
-            
+
+            if (oldVersion == 1 && newVersion == 3) {
+                // upgrade from 1 to 3
+                for (String query : upgradeToV3) {
+                    db.execSQL(query);
+                }
+            }
+
             logger.debug("Database upgraded from " + oldVersion + " to " + newVersion);
         }catch(Exception e){
             logger.error(e);
