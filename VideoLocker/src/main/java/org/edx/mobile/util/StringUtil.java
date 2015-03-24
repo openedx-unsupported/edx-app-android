44512035f7998042c3b649b7840e139e03df491d
shahidtamboli <shahid.tamboli@claricetechnologies.com>
2015-03-24 16:00:49 +0530

    Update StringUtil method to return true if string is empty or null

--- VideoLocker/src/main/java/org/edx/mobile/util/StringUtil.java
+++ VideoLocker/src/main/java/org/edx/mobile/util/StringUtil.java
@@ -3,14 +3,14 @@ package org.edx.mobile.util;
 public class StringUtil {
 
     /**
-     * Returns false if passed string is null or empty
+     * Returns true if passed string is null or empty
      * @param text
      * @return
      */
     public static boolean isStringEmpty(String text) {
         if (text == null || text.isEmpty()){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
