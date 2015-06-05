392404281cea8a06d2267cdf5196cb1f075ca4c9
Jake Lim <jakelim@qti.qualcomm.com>
2015-06-04 17:44:13 -0700

    add json parsing

--- VideoLocker/src/main/java/org/edx/mobile/model/api/DiscussionComment.java
+++ VideoLocker/src/main/java/org/edx/mobile/model/api/DiscussionComment.java
@@ -1,5 +1,11 @@
 package org.edx.mobile.model.api;
 
+import org.edx.mobile.util.DateUtil;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,7 +20,6 @@ public class DiscussionComment {
     String renderedBody;
     String author;
     String authorLabel;
-    boolean flagged = false;
     boolean voted = false;
     int voteCount = 0;
     Date createdAt;
@@ -26,4 +31,43 @@ public class DiscussionComment {
     boolean abuseFlagged = false;
     String editableFields;
     List<DiscussionComment> children;
+
+    public DiscussionComment(JSONObject jsonObj) throws JSONException {
+        identifier = jsonObj.getString("id");
+        parentId = jsonObj.optString("parent_id");
+        threadId = jsonObj.optString("thread_id");
+        rawBody = jsonObj.optString("raw_body");
+        renderedBody = jsonObj.optString("rendered_body");
+        author = jsonObj.optString("author");
+        authorLabel = jsonObj.optString("author_label");
+        voted = jsonObj.optBoolean("voted");
+        voteCount = jsonObj.optInt("vote_count");
+        String dateStr = jsonObj.optString("created_at");
+        if (dateStr != null) {
+            createdAt = DateUtil.convertToDate(dateStr);
+        }
+        dateStr = jsonObj.optString("updated_at");
+        if (dateStr != null) {
+            updatedAt = DateUtil.convertToDate(dateStr);
+        }
+        endorsed = jsonObj.optBoolean("endorsed");
+        endorsedBy = jsonObj.optString("endorsed_by");
+        endorsedByLabel = jsonObj.optString("endorsed_by_label");
+        dateStr = jsonObj.optString("endorsed_at");
+        if (dateStr != null) {
+            endorsedAt = DateUtil.convertToDate(dateStr);
+        }
+        abuseFlagged = jsonObj.optBoolean("abuse_flagged");
+        editableFields = jsonObj.optString("editable_fields");
+        JSONArray jsonArray = jsonObj.optJSONArray("children");
+        if (jsonArray != null) {
+            ArrayList<DiscussionComment> childrenArray = new ArrayList<DiscussionComment>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject childObj = jsonArray.getJSONObject(i);
+                DiscussionComment child = new DiscussionComment(childObj);
+                childrenArray.add(child);
+            }
+            children = childrenArray;
+        }
+    }
 }
--- VideoLocker/src/main/java/org/edx/mobile/model/api/DiscussionThread.java
+++ VideoLocker/src/main/java/org/edx/mobile/model/api/DiscussionThread.java
@@ -1,5 +1,9 @@
 package org.edx.mobile.model.api;
 
+import org.edx.mobile.util.DateUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 
 /**
@@ -10,7 +14,7 @@ public class DiscussionThread {
     String type;
     String courseId;
     String topicId;
-    String groupId;
+    int groupId;
     String groupName;
     String title;
     String rawBody;
@@ -28,4 +32,35 @@ public class DiscussionThread {
     Date createdAt;
     Date updatedAt;
     String editableFields;
+
+    public DiscussionThread(JSONObject jsonObj) throws JSONException {
+        identifier = jsonObj.getString("id");
+        type = jsonObj.optString("type");
+        courseId = jsonObj.optString("course_id");
+        topicId = jsonObj.optString("topic_id");
+        groupId = jsonObj.optInt("group_id");
+        groupName = jsonObj.optString("group_name");
+        title = jsonObj.optString("title");
+        rawBody = jsonObj.optString("raw_body");
+        renderedBody = jsonObj.optString("rendered_body");
+        author = jsonObj.optString("author");
+        authorLabel = jsonObj.optString("author_label");
+        commentListUrl = jsonObj.optString("comment_list_url");
+        hasEndorsed = jsonObj.optBoolean("has_endorsed");
+        pinned = jsonObj.optBoolean("pinned");
+        closed = jsonObj.optBoolean("closed");
+        following = jsonObj.optBoolean("following");
+        abuseFlagged = jsonObj.optBoolean("abuse_flagged");
+        voted = jsonObj.optBoolean("voted");
+        voteCount = jsonObj.optInt("vote_count");
+        String dateStr = jsonObj.optString("created_at");
+        if (dateStr != null) {
+            createdAt = DateUtil.convertToDate(dateStr);
+        }
+        dateStr = jsonObj.optString("updated_at");
+        if (dateStr != null) {
+            updatedAt = DateUtil.convertToDate(dateStr);
+        }
+        editableFields = jsonObj.optString("editable_fields");
+    }
 }
