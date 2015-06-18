d2549ecc90094ec1a32ee8f5e6df23c0057034d7
Ibraheem Zaman <ibraheem@edx.org>
2015-06-18 14:48:13 +0500

    Add sanity checks in getVideos() method in CourseComponent
    The getVideos() method in the CourseComponent model had been edited to
    exclude the leaf blocks that have a type of video, but does not have a
    valid value for the data field in which case it is represented as an
    HtmlBlockModel instead of VideoBlockModel, and thus would potentially
    cause a ClassCastException in this method in that case (which should
    never actually happen in practice, but is present in current server
    response, and thus may be included in the mock response as well).

--- VideoLocker/src/main/java/org/edx/mobile/model/course/CourseComponent.java
+++ VideoLocker/src/main/java/org/edx/mobile/model/course/CourseComponent.java
@@ -8,6 +8,7 @@ import org.edx.mobile.model.api.IPathNode;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
@@ -209,6 +210,18 @@ public class CourseComponent implements IBlock, IPathNode {
     public List<HasDownloadEntry> getVideos(){
         List<CourseComponent> videos = new ArrayList<>();
         fetchAllLeafComponents(videos, EnumSet.of(BlockType.VIDEO));
+        // Confirm that these are actually VideoBlockModel instances.
+        // This is necessary because if for some reason the data is null,
+        // then the block is represented as an HtmlBlockModel, even if
+        // the type is video. This should not actually happen in practice
+        // though; this is just a safeguard to handle that unlikely case.
+        for (Iterator<CourseComponent> videosIterator = videos.iterator();
+             videosIterator.hasNext();) {
+            CourseComponent videoComponent = videosIterator.next();
+            if (!(videoComponent instanceof VideoBlockModel)) {
+                videosIterator.remove();
+            }
+        }
         return (List)videos;
     }
 
