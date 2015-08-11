25d8ce67997b8c7a855d6ed15d6f78fb2134f326
Ibraheem Zaman <ibraheem@edx.org>
2015-08-11 23:15:11 +0500

    Remove redundant check for main thread on network operations
    Commit 9b02037dddda805f71a794b6ce90d732d1416543 added a check for the
    current thread in HttpRequestDelegate in order to verify that the main
    thread was not being used for networking operations. A runtime exception
    was thrown if that was the case.
    
    However, this check is redundant since this is already handled by the
    StrictMode thread policy, with an exception thrown by default when
    targeting Honeycomb or higher. Moreover, since Robolectric currently
    redirects all message loops to the main thread, this causes Robolectric
    tests to fail.
    
    This commit removes this check, and removes the code duplication in the
    Robolectrie tests that were designed to avoid encountering this issue.

--- VideoLocker/src/test/java/org/edx/mobile/test/http/ApiTests.java
+++ VideoLocker/src/test/java/org/edx/mobile/test/http/ApiTests.java
@@ -1,8 +1,6 @@
 package org.edx.mobile.test.http;
 
-import org.apache.http.HttpStatus;
-import org.edx.mobile.http.HttpManager;
-import org.edx.mobile.http.HttpRequestDelegate;
+import org.edx.mobile.http.OkHttpUtil;
 import org.edx.mobile.model.Filter;
 import org.edx.mobile.model.api.AnnouncementsModel;
 import org.edx.mobile.model.api.AuthResponse;
@@ -16,13 +14,9 @@ import org.edx.mobile.model.api.VideoResponseModel;
 import org.edx.mobile.model.course.BlockPath;
 import org.edx.mobile.model.course.BlockType;
 import org.edx.mobile.model.course.CourseComponent;
-import org.edx.mobile.model.course.CourseStructureJsonHandler;
-import org.edx.mobile.model.course.CourseStructureV1Model;
 import org.edx.mobile.model.course.HasDownloadEntry;
 import org.edx.mobile.model.course.IBlock;
 import org.edx.mobile.module.registration.model.RegistrationDescription;
-import org.edx.mobile.services.CourseManager;
-import org.edx.mobile.services.ServiceManager;
 import org.edx.mobile.util.Config;
 import org.junit.Test;
 
@@ -241,29 +235,8 @@ public class ApiTests extends HttpBaseTestCase {
         // General overall testing of CourseComponent API without recursion
         EnrolledCoursesResponse e = api.getEnrolledCourses().get(0);
         final String courseId = e.getCourse().getId();
-        HttpRequestDelegate<CourseComponent> requestDelegate = new HttpRequestDelegate<CourseComponent>(
-                api, null, serviceManager.getEndPointCourseStructure(courseId)) {
-            @Override
-            public CourseComponent fromJson(String json) throws Exception{
-                CourseStructureV1Model model = new CourseStructureJsonHandler().processInput(json);
-                assertNotNull(model);
-                assertNotNull(model.root);
-                assertNotNull(model.blockData);
-                assertNotNull(model.getBlockById(model.root));
-                return (CourseComponent) CourseManager.normalizeCourseStructure(model, courseId);
-            }
-
-            @Override
-            public HttpManager.HttpResult invokeHttpCall() throws Exception {
-                return api.getCourseStructure(this);
-            }
-        };
-        HttpManager.HttpResult result = requestDelegate.invokeHttpCall();
-        assertNotNull(result);
-        assertEquals(HttpStatus.SC_OK, result.statusCode);
-        assertNotNull(result.body);
-
-        final CourseComponent courseComponent = requestDelegate.fromJson(result.body);
+        final CourseComponent courseComponent = serviceManager.getCourseStructure(courseId,
+                OkHttpUtil.REQUEST_CACHE_TYPE.IGNORE_CACHE);
         assertNotNull(courseComponent);
         assertNotNull(courseComponent.getRoot());
         assertEquals(courseId, courseComponent.getCourseId());
