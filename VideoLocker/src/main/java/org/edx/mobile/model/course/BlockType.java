8820705b2302d2c3310f90ef6367525e433010fd
Mian Khalid <mkhalid@edx.org>
2015-08-11 17:01:42 +0500

    Course sections decision is now based on Block Type

--- VideoLocker/src/main/java/org/edx/mobile/model/course/BlockType.java
+++ VideoLocker/src/main/java/org/edx/mobile/model/course/BlockType.java
@@ -4,6 +4,17 @@ package org.edx.mobile.model.course;
  * Created by hanning on 5/21/15.
  */
 public enum  BlockType {
-    COURSE, CHAPTER,  SECTION,  SEQUENTIAL,
-    VERTICAL,  VIDEO,  HTML, PROBLEM, DISCUSSION, OTHERS
+
+    COURSE{ @Override public boolean isContainer() {return true;} },
+    CHAPTER{ @Override public boolean isContainer() {return true;} },
+    SECTION{ @Override public boolean isContainer() {return true;} },
+    SEQUENTIAL{ @Override public boolean isContainer() {return true;} },
+    VERTICAL{ @Override public boolean isContainer() {return true;} },
+    VIDEO{ @Override public boolean isContainer() {return false;} },
+    HTML{ @Override public boolean isContainer() {return false;} },
+    PROBLEM{ @Override public boolean isContainer() {return false;} },
+    DISCUSSION{ @Override public boolean isContainer() {return false;} },
+    OTHERS{ @Override public boolean isContainer() {return false;} };
+
+    abstract boolean isContainer();
 }
