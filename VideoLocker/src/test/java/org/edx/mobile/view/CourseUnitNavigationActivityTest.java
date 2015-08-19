028d99cea9ec57bb4fb5ab397f6b0b50d29c0dd7
Ibraheem Zaman <ibraheem@edx.org>
2015-08-19 17:44:47 +0500

    Add tests for navigating CourseUnitNavigationActivity
    Tests navigation between units as represented by CourseUnitFragment
    subclasses in the ViewPager in CourseUnitNavigationActivity.

--- VideoLocker/src/test/java/org/edx/mobile/view/CourseUnitNavigationActivityTest.java
+++ VideoLocker/src/test/java/org/edx/mobile/view/CourseUnitNavigationActivityTest.java
@@ -4,24 +4,36 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.TextView;
 
 import org.edx.mobile.R;
 import org.edx.mobile.http.OkHttpUtil;
 import org.edx.mobile.model.api.EnrolledCoursesResponse;
 import org.edx.mobile.model.course.BlockType;
 import org.edx.mobile.model.course.CourseComponent;
+import org.edx.mobile.model.course.HtmlBlockModel;
+import org.edx.mobile.model.course.VideoBlockModel;
+import org.edx.mobile.module.prefs.PrefManager;
 import org.edx.mobile.view.custom.DisableableViewPager;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowApplication;
 import org.robolectric.util.ActivityController;
+import org.robolectric.util.Scheduler;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.ListIterator;
 
 import static org.assertj.android.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 // I don't understand the data model cycle behind this, and I couldn't run
@@ -88,16 +100,174 @@ public class CourseUnitNavigationActivityTest extends CourseBaseActivityTest {
     public void initializeTest() {
         super.initializeTest();
 
+        // Change the settings to show all the course content for easier and
+        // more comprehensive testing
+        new PrefManager.UserPrefManager(RuntimeEnvironment.application)
+                .setUserPrefVideoModel(false);
+
+        Intent intent = getIntent();
         ActivityController<? extends CourseUnitNavigationActivity> controller =
-                Robolectric.buildActivity(getActivityClass()).withIntent(getIntent());
+                Robolectric.buildActivity(getActivityClass()).withIntent(intent);
         CourseUnitNavigationActivity activity = controller.get();
 
         controller.create();
         assertNotNull(activity.findViewById(R.id.course_unit_nav_bar));
-        assertNotNull(activity.findViewById(R.id.goto_prev));
-        assertNotNull(activity.findViewById(R.id.goto_next));
+        View prev = activity.findViewById(R.id.goto_prev);
+        assertNotNull(prev);
+        assertThat(prev).isInstanceOf(TextView.class);
+        TextView prevButton = (TextView) prev;
+        View next = activity.findViewById(R.id.goto_next);
+        assertNotNull(next);
+        assertThat(next).isInstanceOf(TextView.class);
+        TextView nextButton = (TextView) next;
+        View prevUnitTitle = activity.findViewById(R.id.prev_unit_title);
+        assertNotNull(prevUnitTitle);
+        assertThat(prevUnitTitle).isInstanceOf(TextView.class);
+        TextView prevUnitLabel = (TextView) prevUnitTitle;
+        View nextUnitTitle = activity.findViewById(R.id.next_unit_title);
+        assertNotNull(nextUnitTitle);
+        assertThat(nextUnitTitle).isInstanceOf(TextView.class);
+        TextView nextUnitLabel = (TextView) nextUnitTitle;
+        View pager = activity.findViewById(R.id.pager);
+        assertNotNull(pager);
+        assertThat(pager).isInstanceOf(ViewPager.class);
+        ViewPager viewPager = (ViewPager) pager;
+        PagerAdapter pagerAdapter = viewPager.getAdapter();
+        assertNotNull(pagerAdapter);
+
+        // Text navigation through units
+        Bundle extras = intent.getBundleExtra(Router.EXTRA_BUNDLE);
+        CourseComponent currentUnit = (CourseComponent)
+                extras.getSerializable(Router.EXTRA_COURSE_UNIT);
+        assertNotNull(currentUnit);
+        List<CourseComponent> units = new ArrayList<>();
+        CourseComponent courseComponent = currentUnit.getRoot();
+        assertNotNull(courseComponent);
+        courseComponent.fetchAllLeafComponents(units,
+                EnumSet.allOf(BlockType.class));
+        assertThat(units).isNotEmpty();
+        controller.start().postCreate((Bundle)null).resume().visible();
+        ListIterator<CourseComponent> unitIterator = units.listIterator(1);
+        for (CourseComponent prevUnit = null;;) {
+            int unitIndex = unitIterator.previousIndex();
+            CourseComponent nextUnit = unitIterator.hasNext() ?
+                    unitIterator.next() : null;
+            verifyState(activity, unitIndex, currentUnit, prevUnit, nextUnit,
+                    viewPager, pagerAdapter, prevButton, nextButton,
+                    prevUnitLabel, nextUnitLabel);
+            if (nextUnit == null) break;
+            // The Scheduler needs to be paused while clicking the next button
+            // to enable the FragmentStatePagerAdapter to clear it's transaction
+            // state, and thus avoid the commit being called recursively
+            Scheduler foregroundScheduler = ShadowApplication.getInstance()
+                    .getForegroundThreadScheduler();
+            foregroundScheduler.pause();
+            assertTrue(nextButton.performClick());
+            foregroundScheduler.unPause();
+            prevUnit = currentUnit;
+            currentUnit = nextUnit;
+        }
+        // Now iterate back in reverse order to test the previous button
+        unitIterator = units.listIterator(units.size() - 1);
+        for (CourseComponent nextUnit = null;;) {
+            int unitIndex = unitIterator.nextIndex();
+            CourseComponent prevUnit = unitIterator.hasPrevious() ?
+                    unitIterator.previous() : null;
+            verifyState(activity, unitIndex, currentUnit, prevUnit, nextUnit,
+                    viewPager, pagerAdapter, prevButton, nextButton,
+                    prevUnitLabel, nextUnitLabel);
+            if (prevUnit == null) break;
+            Scheduler foregroundScheduler = ShadowApplication.getInstance()
+                    .getForegroundThreadScheduler();
+            foregroundScheduler.pause();
+            assertTrue(prevButton.performClick());
+            foregroundScheduler.unPause();
+            nextUnit = currentUnit;
+            currentUnit = prevUnit;
+        }
     }
 
+    /**
+     * Testing download progress menu visibility states and click behaviour
+     * (starting DownloadActivity). Only when both AppConstants.offline_flag
+     * is true and there is a downloading entry in the database, should the
+     * progress bar be visible.
+     */
+    /**
+     * Generic method for verifying the state of the CourseUnitNavigationActivity
+     * at a specific unit.
+     *
+     * @param activity An instance of CourseUnitNavigationActivity that has been
+     *                 initialized
+     * @param unitIndex The index of the unit among all the leaves for the course
+     * @param currentUnit The selected course unit
+     * @param prevUnit The unit previous to the current selection
+     * @param nextUnit The unit next to the current selection
+     * @param viewPager The ViewPager instance containing the CourseUnitFragment
+     *                  instances
+     * @param pagerAdapter The PagerAdapter associated with the ViewPager
+     * @param prevButton The button for going to the previous unit
+     * @param nextButton The button for going to the next unit
+     * @param prevUnitLabel The label for the previous unit
+     * @param nextUnitLabel The label for the next unit
+     */
+    private void verifyState(CourseUnitNavigationActivity activity,
+            int unitIndex, CourseComponent currentUnit,
+            CourseComponent prevUnit, CourseComponent nextUnit,
+            ViewPager viewPager, PagerAdapter pagerAdapter,
+            TextView prevButton, TextView nextButton,
+            TextView prevUnitLabel, TextView nextUnitLabel) {
+        assertTitle(activity, currentUnit.getDisplayName());
+
+        Class<? extends CourseUnitFragment> fragmentClass;
+        if (currentUnit instanceof VideoBlockModel) {
+            fragmentClass = CourseUnitVideoFragment.class;
+        } else if (!currentUnit.isResponsiveUI() ){
+            fragmentClass = CourseUnitMobileNotSupportedFragment.class;
+        } else if (currentUnit.getType() != BlockType.VIDEO &&
+                currentUnit.getType() != BlockType.HTML &&
+                currentUnit.getType() != BlockType.OTHERS &&
+                currentUnit.getType() != BlockType.DISCUSSION &&
+                currentUnit.getType() != BlockType.PROBLEM ) {
+            fragmentClass = CourseUnitEmptyFragment.class;
+        } else if (currentUnit instanceof HtmlBlockModel) {
+            fragmentClass = CourseUnitWebviewFragment.class;
+        } else {
+            fragmentClass = CourseUnitMobileNotSupportedFragment.class;
+        }
+        Object item = pagerAdapter.instantiateItem(viewPager, unitIndex);
+        assertNotNull(item);
+        assertThat(item).isInstanceOf(fragmentClass);
+        Bundle args = ((Fragment) item).getArguments();
+        assertNotNull(args);
+        assertEquals(currentUnit, args.getSerializable(
+                Router.EXTRA_COURSE_UNIT));
+
+        assertEquals(prevUnit != null, prevButton.isEnabled());
+        assertEquals(nextUnit != null, nextButton.isEnabled());
+        CourseComponent prevSection = prevUnit == null ?
+                null : prevUnit.getParent();
+        CourseComponent nextSection = nextUnit == null ?
+                null : nextUnit.getParent();
+        if (prevSection == null ||
+                currentUnit.getParent().equals(prevSection)) {
+            assertThat(prevUnitLabel).isNotVisible();
+            assertThat(prevButton).hasText(R.string.assessment_previous);
+        } else {
+            assertThat(prevUnitLabel).isVisible();
+            assertThat(prevUnitLabel).hasText(prevSection.getName());
+            assertThat(prevButton).hasText(R.string.assessment_previous_unit);
+        }
+        if (nextSection == null ||
+                currentUnit.getParent().equals(nextSection)) {
+            assertThat(nextUnitLabel).isNotVisible();
+            assertThat(nextButton).hasText(R.string.assessment_next);
+        } else {
+            assertThat(nextUnitLabel).isVisible();
+            assertThat(nextUnitLabel).hasText(nextSection.getName());
+            assertThat(nextButton).hasText(R.string.assessment_next_unit);
+        }
+    }
     /**
      * Generic method for asserting proper setup for the current orientation
      *
