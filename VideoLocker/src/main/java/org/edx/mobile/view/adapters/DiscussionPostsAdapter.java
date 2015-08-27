6b3bba61417d7348321fb5ccd567d014bfe3f3b1
Brian Guertin <bguertin@edx.org>
2015-08-27 14:58:35 -0400

    fix pinned / following labels

--- VideoLocker/src/main/java/org/edx/mobile/view/adapters/DiscussionPostsAdapter.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/adapters/DiscussionPostsAdapter.java
@@ -1,35 +1,27 @@
 package org.edx.mobile.view.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.inject.Inject;
-import org.edx.mobile.discussion.DiscussionThread;
-import org.edx.mobile.discussion.PinnedAuthor;
 
 import org.edx.mobile.R;
 import org.edx.mobile.core.IEdxEnvironment;
+import org.edx.mobile.discussion.DiscussionThread;
+import org.edx.mobile.discussion.PinnedAuthor;
+import org.edx.mobile.third_party.iconify.IconDrawable;
 import org.edx.mobile.third_party.iconify.IconView;
 import org.edx.mobile.third_party.iconify.Iconify;
-import org.edx.mobile.view.Router;
 
 public class DiscussionPostsAdapter extends BaseListAdapter<DiscussionThread> {
 
     @Inject
-    Router router;
-
-    private final Context context;
-
-
-
-    @Inject
     public DiscussionPostsAdapter(Context context, IEdxEnvironment environment) {
         super(context, R.layout.row_discussion_thread, environment);
-        this.context = context;
     }
 
     @Override
@@ -46,60 +38,52 @@ public class DiscussionPostsAdapter extends BaseListAdapter<DiscussionThread> {
         String threadTitle = discussionThread.getTitle();
         holder.discussionPostTitle.setText(threadTitle);
 
-        if (discussionThread.isPinned() || discussionThread.isFollowing()) {
-            holder.discussionPostPinFollowIcon.setVisibility(View.VISIBLE);
-            holder.discussionPostPinFollowTextView.setVisibility(View.VISIBLE);
-
-            Iconify.IconValue pinFollowIcon = discussionThread.isPinned() ?
-                    Iconify.IconValue.fa_thumb_tack : Iconify.IconValue.fa_star;
-            holder.discussionPostPinFollowIcon.setIcon(pinFollowIcon);
-
+        if (discussionThread.getAuthorLabel() != null) {
+            holder.discussionPostPinTextView.setVisibility(View.VISIBLE);
             String pinFollowTextLabel = "";
-            if (discussionThread.isFollowing()) {
-                pinFollowTextLabel = context.getString(R.string.discussion_posts_following);
-
-            } else if (discussionThread.getAuthorLabel() == PinnedAuthor.STAFF) {
-                pinFollowTextLabel = context.getString(R.string.discussion_priviledged_author_label_staff);
+            if (discussionThread.getAuthorLabel() == PinnedAuthor.STAFF) {
+                pinFollowTextLabel = getContext().getString(R.string.discussion_priviledged_author_label_staff);
 
             } else if (discussionThread.getAuthorLabel() == PinnedAuthor.COMMUNITY_TA) {
-                pinFollowTextLabel = context.getString(R.string.discussion_priviledged_author_label_ta);
+                pinFollowTextLabel = getContext().getString(R.string.discussion_priviledged_author_label_ta);
             }
-            holder.discussionPostPinFollowTextView.setText(pinFollowTextLabel);
+            final Drawable icon = discussionThread.isPinned() ?
+                    new IconDrawable(getContext(), Iconify.IconValue.fa_thumb_tack).colorRes(R.color.edx_grayscale_neutral_base).sizeRes(R.dimen.edx_x_small)
+                    : null;
+            holder.discussionPostPinTextView.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
+            holder.discussionPostPinTextView.setText(pinFollowTextLabel);
 
-            // If the discussion thread has an extra line, add 25dp to row height
-            // TODO: Add the additional height if the title spans 2 lines
-            AbsListView.LayoutParams params = (AbsListView.LayoutParams) holder.discussionPostRow.getLayoutParams();
-            int boxHeight = context.getResources().getDimensionPixelSize(
-                    R.dimen.discussion_post_box_height);
-            int extraHeight = context.getResources().getDimensionPixelSize(
-                    R.dimen.discussion_post_extra_box_height);
-            params.height = boxHeight + extraHeight;
-            holder.discussionPostRow.setLayoutParams(params);
+        } else {
+            holder.discussionPostPinTextView.setVisibility(View.GONE);
         }
 
-        int commentColor = discussionThread.getUnreadCommentCount() == 0 ?
+        if (discussionThread.isFollowing()) {
+            holder.discussionPostFollowTextView.setVisibility(View.VISIBLE);
+            holder.discussionPostFollowTextView.setCompoundDrawablesWithIntrinsicBounds(
+                    new IconDrawable(getContext(), Iconify.IconValue.fa_star).colorRes(R.color.edx_grayscale_neutral_base).sizeRes(R.dimen.edx_x_small), null, null, null);
+        } else {
+            holder.discussionPostFollowTextView.setVisibility(View.GONE);
+        }
+
+        final int commentColor = discussionThread.getUnreadCommentCount() == 0 ?
                 R.color.edx_grayscale_neutral_light : R.color.edx_brand_primary_base;
         holder.discussionPostNumCommentsTextView.setText(Integer.toString(discussionThread.getCommentCount()));
-        holder.discussionPostNumCommentsTextView.setTextColor(context.getResources().getColor(commentColor));
+        holder.discussionPostNumCommentsTextView.setTextColor(getContext().getResources().getColor(commentColor));
         holder.discussionPostCommentIcon.setIcon(Iconify.IconValue.fa_comment);
-        holder.discussionPostCommentIcon.setIconColor(context.getResources().getColor(commentColor));
-
-        // TODO: Read property is not being returned in API to get the thread list
-        if (discussionThread.isRead()) {
-            holder.discussionPostRow.setBackgroundColor(context.getResources().getColor(
-                    R.color.edx_grayscale_neutral_xx_light));
-        }
+        holder.discussionPostCommentIcon.setIconColor(getContext().getResources().getColor(commentColor));
+        holder.discussionPostRow.setBackgroundColor(getContext().getResources().getColor(
+                discussionThread.isRead() ? R.color.edx_grayscale_neutral_xx_light : R.color.edx_grayscale_neutral_white));
     }
 
     @Override
     public BaseViewHolder getTag(View convertView) {
-        ViewHolder holder = new ViewHolder();
+        final ViewHolder holder = new ViewHolder();
 
         holder.discussionPostRow = (RelativeLayout) convertView.findViewById(R.id.row_discussion_post_relative_layout);
         holder.discussionPostTypeIcon = (IconView) convertView.findViewById(R.id.discussion_post_type_icon);
         holder.discussionPostTitle = (TextView) convertView.findViewById(R.id.discussion_post_title);
-        holder.discussionPostPinFollowIcon = (IconView) convertView.findViewById(R.id.discussion_post_pin_or_following_icon);
-        holder.discussionPostPinFollowTextView = (TextView) convertView.findViewById(R.id.discussion_post_pin_or_following_text_view);
+        holder.discussionPostPinTextView = (TextView) convertView.findViewById(R.id.discussion_post_pin_text_view);
+        holder.discussionPostFollowTextView = (TextView) convertView.findViewById(R.id.discussion_post_following_text_view);
         holder.discussionPostNumCommentsTextView = (TextView) convertView.findViewById(R.id.discussion_post_num_comments_text_view);
         holder.discussionPostCommentIcon = (IconView) convertView.findViewById(R.id.discussion_post_comment_icon);
 
@@ -107,14 +91,15 @@ public class DiscussionPostsAdapter extends BaseListAdapter<DiscussionThread> {
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {}
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    }
 
     private static class ViewHolder extends BaseViewHolder {
         RelativeLayout discussionPostRow;
         IconView discussionPostTypeIcon;
         TextView discussionPostTitle;
-        IconView discussionPostPinFollowIcon;
-        TextView discussionPostPinFollowTextView;
+        TextView discussionPostPinTextView;
+        TextView discussionPostFollowTextView;
         TextView discussionPostNumCommentsTextView;
         IconView discussionPostCommentIcon;
 
