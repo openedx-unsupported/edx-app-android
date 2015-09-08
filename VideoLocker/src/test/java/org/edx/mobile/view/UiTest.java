8f7e13b7c4811d73ad2bca3d1ab870faff1a28a7
Ibraheem Zaman <ibraheem@edx.org>
2015-09-08 23:34:33 +0500

    Ignore abstract base UI test suite
    The UiTest class was introduced in commit
    67a80bc8026b51a81d54c6dbb6207399df4c8eee as an abstract super class for
    all the UI test suites for the purpose of defining shared dependencies
    for UI tests. However, since it inherits from HttpBaseTestCase and does
    not explicitly defines itself to be ignored in the tests, the test
    runner was reporting an error on it upon encountering no test methods in
    it. This commit explicitly marks this class to be ignored in the tests
    to fix this error.

--- VideoLocker/src/test/java/org/edx/mobile/view/UiTest.java
+++ VideoLocker/src/test/java/org/edx/mobile/view/UiTest.java
@@ -2,10 +2,12 @@ package org.edx.mobile.view;
 
 import org.edx.mobile.test.http.HttpBaseTestCase;
 import org.junit.Before;
+import org.junit.Ignore;
 
 /**
  * Base class for all UI test suites.
  */
+@Ignore
 public class UiTest extends HttpBaseTestCase {
     /**
      * Ensure login before tests.
