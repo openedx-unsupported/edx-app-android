e617f224b782d5a936f6f63d3d31a9651aeeb9c5
Ibraheem Zaman <ibraheem@edx.org>
2015-10-16 04:21:34 +0500

    Delegate course content links to external browsers
    All links in the course content are now delegated to external browsers,
    using the existing URLInterceptorWebViewClient utility client. The
    utility class has also been cleaned up along with the external methods
    used in it, and fixed to make it's Config injection work.

--- VideoLocker/src/main/java/org/edx/mobile/view/CertificateFragment.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/CertificateFragment.java
@@ -19,7 +19,6 @@ import org.edx.mobile.logger.Logger;
 import org.edx.mobile.model.api.EnrolledCoursesResponse;
 import org.edx.mobile.module.analytics.ISegment;
 import org.edx.mobile.social.facebook.FacebookProvider;
-import org.edx.mobile.util.BrowserUtil;
 import org.edx.mobile.util.SocialUtils;
 import org.edx.mobile.view.custom.URLInterceptorWebViewClient;
 import org.edx.mobile.view.dialog.InstallFacebookDialog;
@@ -78,13 +77,7 @@ public class CertificateFragment extends RoboFragment {
         progressBar = (ProgressBar) view.findViewById(R.id.api_spinner);
 
         webview = (WebView) view.findViewById(R.id.webview);
-        new URLInterceptorWebViewClient(webview) {
-
-            @Override
-            public void onOpenExternalURL(String url) {
-                BrowserUtil.open(getActivity(), url);
-            }
-        };
+        new URLInterceptorWebViewClient(getActivity(), webview);
 
         facebookShare = (LinearLayout) view.findViewById(R.id.share_certificate);
         facebookShare.setOnClickListener( new View.OnClickListener() {
