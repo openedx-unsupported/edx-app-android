ceea43822854bd086cbf0794bcbb81c29b3e9e97
Brian Guertin <bguertin@edx.org>
2015-10-26 14:04:11 -0400

    Starting implementing crop overlay view

--- VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
@@ -7,11 +7,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.widget.ImageView;
 
-import com.theartofdev.edmodo.cropper.CropImageView;
+import com.bumptech.glide.Glide;
 
 import org.edx.mobile.R;
+
+import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class CropImageActivity extends Activity {
     public static final String EXTRA_IMAGE_URI = "imageUri";
@@ -25,12 +26,8 @@ public class CropImageActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop_image);
-        final CropImageView cropImageView = (CropImageView) findViewById(R.id.crop_image_view);
-        cropImageView.setImageUri((Uri) getIntent().getParcelableExtra(EXTRA_IMAGE_URI));
-        cropImageView.setCropShape(CropImageView.CropShape.OVAL);
-        cropImageView.setAspectRatio(1, 1);
-        cropImageView.setFixedAspectRatio(true);
-        cropImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        cropImageView.setGuidelines(2);
+        final ImageViewTouch imageView = (ImageViewTouch) findViewById(R.id.image);
+        final Uri imageUri = getIntent().getParcelableExtra(EXTRA_IMAGE_URI);
+        Glide.with(this).load(imageUri).dontAnimate().into(imageView);
     }
 }
---
+++ VideoLocker/src/main/java/org/edx/mobile/view/custom/popup/CropOverlay.java
@@ -0,0 +1,58 @@
+package org.edx.mobile.view.custom.popup;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Region;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CropOverlay extends View {
+
+    private Paint borderPaint;
+    private Paint backgroundPaint;
+
+    final Path circleSelectionPath = new Path();
+    final RectF mRectF = new RectF();
+
+    public CropOverlay(Context context) {
+        this(context, null);
+    }
+
+    public CropOverlay(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CropOverlay(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public CropOverlay(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        borderPaint = new Paint();
+        borderPaint.setColor(Color.WHITE);
+        borderPaint.setStrokeWidth(3 * context.getResources().getDisplayMetrics().density);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.argb(100, 0, 0, 0));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Draw background with transparent circular hole
+        final float radius = (Math.min(canvas.getWidth(), canvas.getHeight()) - (10 * getResources().getDisplayMetrics().density)) / 2;
+        mRectF.set(canvas.getWidth() / 2 - radius, canvas.getHeight() / 2 - radius, canvas.getWidth() / 2 + radius, canvas.getHeight() / 2 + radius);
+        circleSelectionPath.addOval(mRectF, Path.Direction.CW);
+        canvas.clipPath(circleSelectionPath, Region.Op.XOR);
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+        canvas.restore();
+
+        // Draw circle border
+        canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, radius, borderPaint);
+    }
+}
