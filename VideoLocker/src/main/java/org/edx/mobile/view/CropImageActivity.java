59b58e9df2482e4975915673483d7b4509242533
Brian Guertin <bguertin@edx.org>
2015-10-26 14:49:49 -0400

    Set minimum scale so crop is always valid

--- VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
@@ -9,10 +9,14 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import org.edx.mobile.R;
 
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
+import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
 
 public class CropImageActivity extends Activity {
     public static final String EXTRA_IMAGE_URI = "imageUri";
@@ -27,7 +31,22 @@ public class CropImageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop_image);
         final ImageViewTouch imageView = (ImageViewTouch) findViewById(R.id.image);
+        imageView.setDisplayType(ImageViewTouchBase.DisplayType.NONE);
         final Uri imageUri = getIntent().getParcelableExtra(EXTRA_IMAGE_URI);
-        Glide.with(this).load(imageUri).dontAnimate().into(imageView);
+        Glide.with(this).load(imageUri).dontAnimate().listener(new RequestListener<Uri, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, Uri model, Target<GlideDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                final float minZoom = Math.max(
+                        (float)resource.getIntrinsicWidth() / imageView.getWidth(),
+                        (float)resource.getIntrinsicHeight() / imageView.getHeight());
+                imageView.setImageDrawable(resource, null, minZoom, 10.0f);
+                return true;
+            }
+        }).into(imageView);
     }
 }
