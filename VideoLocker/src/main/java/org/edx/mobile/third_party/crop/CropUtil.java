99cf0d781280e956688af6d9e7d9b4d4da9f5749
Brian Guertin <contact@brianguertin.com>
2015-10-29 12:53:12 -0400

    Fix EXIF rotation handling crash

--- VideoLocker/src/main/java/org/edx/mobile/third_party/crop/CropUtil.java
+++ VideoLocker/src/main/java/org/edx/mobile/third_party/crop/CropUtil.java
@@ -17,7 +17,6 @@
 
 package org.edx.mobile.third_party.crop;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -28,152 +27,65 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.util.Log;
 
-import java.io.Closeable;
 import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /*
- * Modified from https://github.com/jdamcd/android-crop which was modified from AOSP
+ * Modified from https://github.com/jdamcd/android-crop which was itself modified from AOSP
  */
 public class CropUtil {
 
-    private static final String SCHEME_FILE = "file";
-    private static final String SCHEME_CONTENT = "content";
-
-    public static void closeSilently(@Nullable Closeable c) {
-        if (c == null) return;
-        try {
-            c.close();
-        } catch (Throwable t) {
-            // Do nothing
+    public static int getExifRotation(int rotation) {
+        switch (rotation) {
+            case 90:
+                return ExifInterface.ORIENTATION_ROTATE_90;
+            case 180:
+                return ExifInterface.ORIENTATION_ROTATE_180;
+            case 270:
+                return ExifInterface.ORIENTATION_ROTATE_270;
+            default:
+                return ExifInterface.ORIENTATION_UNDEFINED;
         }
     }
 
-    public static int getExifRotation(File imageFile) {
-        if (imageFile == null) return 0;
-        try {
-            ExifInterface exif = new ExifInterface(imageFile.getAbsolutePath());
-            // We only recognize a subset of orientation tag values
-            switch (exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)) {
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    return 90;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    return 180;
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    return 270;
-                default:
-                    return ExifInterface.ORIENTATION_UNDEFINED;
+    public static void setExifOrientation(int rotation, @NonNull File destFile) throws IOException {
+        ExifInterface exif = new ExifInterface(destFile.getAbsolutePath());
+        if (rotation != 0) {
+            exif.setAttribute(ExifInterface.TAG_ORIENTATION, String.valueOf(getExifRotation(rotation)));
+            exif.saveAttributes();
+        }
+    }
+
+    public static int getOrientation(@NonNull Context context, @NonNull Uri photoUri) {
+        try (Cursor cursor = context.getContentResolver().query(photoUri,
+                new String[]{MediaStore.Images.ImageColumns.ORIENTATION}, null, null, null)) {
+
+            if (null == cursor || cursor.getCount() != 1) {
+                return -1;
             }
-        } catch (IOException e) {
-            Log.e(CropUtil.class.getSimpleName(), "Error getting Exif data", e);
-            return 0;
+
+            cursor.moveToFirst();
+            return cursor.getInt(0);
         }
     }
 
-    public static void copyExifRotation(@NonNull File sourceFile, @NonNull File destFile) throws IOException {
-        ExifInterface exifSource = new ExifInterface(sourceFile.getAbsolutePath());
-        ExifInterface exifDest = new ExifInterface(destFile.getAbsolutePath());
-        final String orientation = exifSource.getAttribute(ExifInterface.TAG_ORIENTATION);
-        if (orientation != null) {
-            exifDest.setAttribute(ExifInterface.TAG_ORIENTATION, orientation);
-            exifDest.saveAttributes();
-        }
-    }
-
-    @Nullable
-    public static File getFromMediaUri(Context context, ContentResolver resolver, Uri uri) {
-        if (uri == null) return null;
-
-        if (SCHEME_FILE.equals(uri.getScheme())) {
-            return new File(uri.getPath());
-        } else if (SCHEME_CONTENT.equals(uri.getScheme())) {
-            final String[] filePathColumn = {MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.DISPLAY_NAME};
-            Cursor cursor = null;
-            try {
-                cursor = resolver.query(uri, filePathColumn, null, null, null);
-                if (cursor != null && cursor.moveToFirst()) {
-                    final int columnIndex = (uri.toString().startsWith("content://com.google.android.gallery3d")) ?
-                            cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME) :
-                            cursor.getColumnIndex(MediaStore.MediaColumns.DATA);
-                    // Picasa images on API 13+
-                    if (columnIndex != -1) {
-                        String filePath = cursor.getString(columnIndex);
-                        if (!TextUtils.isEmpty(filePath)) {
-                            return new File(filePath);
-                        }
-                    }
-                }
-            } catch (IllegalArgumentException e) {
-                // Google Drive images
-                return getFromMediaUriPfd(context, resolver, uri);
-            } catch (SecurityException ignored) {
-                // Nothing we can do
-            } finally {
-                if (cursor != null) cursor.close();
-            }
-        }
-        return null;
-    }
-
-    private static String getTempFilename(Context context) throws IOException {
-        File outputDir = context.getCacheDir();
-        File outputFile = File.createTempFile("image", "tmp", outputDir);
-        return outputFile.getAbsolutePath();
-    }
-
-    @Nullable
-    private static File getFromMediaUriPfd(Context context, ContentResolver resolver, Uri uri) {
-        if (uri == null) return null;
-
-        FileInputStream input = null;
-        FileOutputStream output = null;
-        try {
-            ParcelFileDescriptor pfd = resolver.openFileDescriptor(uri, "r");
-            FileDescriptor fd = pfd.getFileDescriptor();
-            input = new FileInputStream(fd);
-
-            String tempFilename = getTempFilename(context);
-            output = new FileOutputStream(tempFilename);
-
-            int read;
-            byte[] bytes = new byte[4096];
-            while ((read = input.read(bytes)) != -1) {
-                output.write(bytes, 0, read);
-            }
-            return new File(tempFilename);
-        } catch (IOException ignored) {
-            // Nothing we can do
-        } finally {
-            closeSilently(input);
-            closeSilently(output);
-        }
-        return null;
-    }
-
-    public static Bitmap decodeRegionCrop(@NonNull Context context, @NonNull Uri sourceUri, Rect rect, int outWidth, int outHeight) throws IOException, OutOfMemoryError {
+    public static Bitmap decodeRegionCrop(@NonNull Context context, @NonNull Uri sourceUri, Rect rect, int outWidth, int outHeight, int rotation) throws IOException, OutOfMemoryError {
         Bitmap croppedImage;
         try (InputStream is = context.getContentResolver().openInputStream(sourceUri)) {
             BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(is, false);
             final int width = decoder.getWidth();
             final int height = decoder.getHeight();
 
-            int exifRotation = getExifRotation(getFromMediaUri(context, context.getContentResolver(), sourceUri));
-
-            if (exifRotation != 0) {
+            if (rotation != 0) {
                 // Adjust crop area to account for image rotation
                 Matrix matrix = new Matrix();
-                matrix.setRotate(-exifRotation);
+                matrix.setRotate(-rotation);
 
                 RectF adjusted = new RectF();
                 matrix.mapRect(adjusted, new RectF(rect));
@@ -193,9 +105,27 @@ public class CropUtil {
             } catch (IllegalArgumentException e) {
                 // Rethrow with some extra information
                 throw new IllegalArgumentException("Rectangle " + rect + " is outside of the image ("
-                        + width + "," + height + "," + exifRotation + ")", e);
+                        + width + "," + height + "," + rotation + ")", e);
             }
         }
         return croppedImage;
+    }
+
+    public static void crop(@NonNull Context context, @NonNull Uri uri, @NonNull Rect cropRect, int width, int height, @NonNull File file) throws IOException {
+        int rotation = getOrientation(context, uri);
+        final Bitmap croppedImage = CropUtil.decodeRegionCrop(context, uri, cropRect, width, height, rotation);
+        try {
+            try (OutputStream outputStream = new FileOutputStream(file)) {
+                croppedImage.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+            }
+        } finally {
+            croppedImage.recycle();
+        }
+
+        CropUtil.setExifOrientation(
+                rotation,
+                file
+        );
+
     }
 }
