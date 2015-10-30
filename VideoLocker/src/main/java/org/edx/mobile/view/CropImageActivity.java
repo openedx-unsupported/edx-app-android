9812f6087ef4233f51cc8eaeaf7f3964a9eee300
Brian Guertin <bguertin@edx.org>
2015-10-30 17:24:29 -0400

    Support choosing images from apps that do not expose files directly

--- VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/CropImageActivity.java
@@ -3,9 +3,7 @@ package org.edx.mobile.view;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Matrix;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,17 +12,19 @@ import android.view.View;
 import android.view.Window;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
 import org.edx.mobile.R;
+import org.edx.mobile.user.SaveUriToFileTask;
 import org.edx.mobile.view.custom.CropImageView;
 
-import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
+import roboguice.activity.RoboActivity;
 
-public class CropImageActivity extends Activity {
+public class CropImageActivity extends RoboActivity {
     public static final String EXTRA_IMAGE_URI = "imageUri";
     public static final String EXTRA_CROP_RECT = "cropRect";
 
@@ -43,6 +43,8 @@ public class CropImageActivity extends Activity {
         return data.getParcelableExtra(EXTRA_CROP_RECT);
     }
 
+    private SaveUriToFileTask task;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,25 +52,52 @@ public class CropImageActivity extends Activity {
         setContentView(R.layout.activity_crop_image);
         final CropImageView imageView = (CropImageView) findViewById(R.id.image);
         imageView.setDisplayType(ImageViewTouchBase.DisplayType.NONE);
-        final Uri imageUri = getIntent().getParcelableExtra(EXTRA_IMAGE_URI);
-        Glide.with(this).load(imageUri).dontAnimate().listener(new RequestListener<Uri, GlideDrawable>() {
+
+        task = new SaveUriToFileTask(this, (Uri) getIntent().getParcelableExtra(EXTRA_IMAGE_URI)) {
             @Override
-            public boolean onException(Exception e, Uri model, Target<GlideDrawable> target, boolean isFirstResource) {
-                return false;
+            protected void onSuccess(final Uri imageUri) throws Exception {
+                Glide.with(CropImageActivity.this)
+                        .load(imageUri)
+                        .dontAnimate()
+                        .skipMemoryCache(true)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .listener(new RequestListener<Uri, GlideDrawable>() {
+                            @Override
+                            public boolean onException(Exception e, Uri model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                // TODO: account for padding around crop circle
+                                final float viewportWidth = (float) imageView.getWidth() - getResources().getDimensionPixelSize(R.dimen.widget_margin) * 2;
+                                final float minZoom = Math.max(
+                                        viewportWidth / resource.getIntrinsicWidth(),
+                                        viewportWidth / resource.getIntrinsicHeight())
+                                        / ((float) imageView.getWidth() / resource.getIntrinsicWidth());
+                                imageView.setImageDrawable(resource, null, minZoom, minZoom * 3);
+                                return true;
+                            }
+                        }).into(imageView);
+
+                findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        setResult(Activity.RESULT_OK, new Intent()
+                                        .putExtra(EXTRA_CROP_RECT, imageView.getCropRect())
+                                        .putExtra(EXTRA_IMAGE_URI, imageUri)
+                        );
+                        finish();
+                    }
+                });
             }
 
             @Override
-            public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-                // TODO: account for padding around crop circle
-                final float viewportWidth = (float)imageView.getWidth() - getResources().getDimensionPixelSize(R.dimen.widget_margin) * 2;
-                final float minZoom = Math.max(
-                        viewportWidth / resource.getIntrinsicWidth(),
-                        viewportWidth / resource.getIntrinsicHeight())
-                        / ((float)imageView.getWidth() / resource.getIntrinsicWidth());
-                imageView.setImageDrawable(resource, null, minZoom, minZoom * 3);
-                return true;
+            protected void onException(Exception e) throws RuntimeException {
+                super.onException(e);
             }
-        }).into(imageView);
+        };
+        task.execute();
 
         findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,13 +105,13 @@ public class CropImageActivity extends Activity {
                 onBackPressed();
             }
         });
+    }
 
-        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setResult(Activity.RESULT_OK, getIntent().putExtra(EXTRA_CROP_RECT, imageView.getCropRect()));
-                finish();
-            }
-        });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != task) {
+            task.cancel(true);
+        }
     }
 }
