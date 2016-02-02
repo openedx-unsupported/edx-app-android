openedx-unsupported/edx-app-android

commit bd6ae85604930a35dead0efa43127404a0d8bcfc

Star icon addition to the posts following screen

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsActivity.java
@@ -2,12 +2,17 @@ package org.edx.mobile.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 
 import com.google.inject.Inject;
-import org.edx.mobile.discussion.DiscussionTopic;
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.fonts.FontAwesomeIcons;
 
 import org.edx.mobile.R;
 import org.edx.mobile.base.BaseSingleFragmentActivity;
+import org.edx.mobile.discussion.DiscussionTopic;
 import org.edx.mobile.model.api.EnrolledCoursesResponse;
 
 import roboguice.inject.InjectExtra;
@@ -63,9 +68,21 @@ public class CourseDiscussionPostsActivity extends BaseSingleFragmentActivity  {
         }
 
         if (discussionTopic != null && discussionTopic.getName() != null) {
-            setTitle(discussionTopic.getName());
+            if (discussionTopic.getIdentifier().equals(DiscussionTopic.FOLLOWING_TOPICS_ID)) {
+                SpannableString title = new SpannableString("   " + discussionTopic.getName());
+                IconDrawable starIcon = new IconDrawable(this, FontAwesomeIcons.fa_star)
+                        .colorRes(this, R.color.edx_grayscale_neutral_white_t)
+                        .sizeRes(this, R.dimen.edx_base);
+                starIcon.setBounds(0, 0, starIcon.getIntrinsicWidth(), starIcon.getIntrinsicHeight());
+                starIcon.setTintList(null);
+                ImageSpan iSpan = new ImageSpan(starIcon, ImageSpan.ALIGN_BASELINE);
+                title.setSpan(iSpan, 0, 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                setTitle(title);
+            }
+            else {
+                setTitle(discussionTopic.getName());
+            }
         }
-
     }
 
 }

commit a3e4d91b95735015d97351f557533ba0cd8f5b69

Fixed crash on navigation of parent topic
Crash was introduced in bd6ae85, where we check equality based on
identifier of a topic. This identifier is null incase of parent topics,
causing the crash.

VideoLocker/src/main/java/org/edx/mobile/discussion/DiscussionTopic.java
@@ -17,6 +17,7 @@
 package org.edx.mobile.discussion;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.google.gson.annotations.SerializedName;
@@ -34,6 +35,14 @@ public class DiscussionTopic implements Serializable {
     String threadListUrl = "";
     List<DiscussionTopic> children = new ArrayList<>();
 
+    /**
+     * Returns the identifier for a discussion topic.
+     * <br>
+     * NOTE: The identifier for a parent topic is always null.
+     *
+     * @return The identifier for a discussion topic.
+     */
+    @Nullable
     public String getIdentifier() {
         return identifier;
     }
@@ -67,7 +76,7 @@ public class DiscussionTopic implements Serializable {
     }
 
     public boolean hasSameId(@NonNull DiscussionTopic discussionTopic) {
-        return discussionTopic.getIdentifier().equals(identifier);
+        return identifier != null && identifier.equals(discussionTopic.getIdentifier());
     }
 
     public boolean containsThread(@NonNull DiscussionThread discussionThread) {

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsActivity.java
@@ -68,7 +68,7 @@ public class CourseDiscussionPostsActivity extends BaseSingleFragmentActivity  {
         }
 
         if (discussionTopic != null && discussionTopic.getName() != null) {
-            if (discussionTopic.getIdentifier().equals(DiscussionTopic.FOLLOWING_TOPICS_ID)) {
+            if (DiscussionTopic.FOLLOWING_TOPICS_ID.equals(discussionTopic.getIdentifier())) {
                 SpannableString title = new SpannableString("   " + discussionTopic.getName());
                 IconDrawable starIcon = new IconDrawable(this, FontAwesomeIcons.fa_star)
                         .colorRes(this, R.color.edx_grayscale_neutral_white_t)