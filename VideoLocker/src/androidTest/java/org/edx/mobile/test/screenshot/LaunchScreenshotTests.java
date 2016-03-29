af73e5e747b5e8198c211a6f9646347518eda498
christopher lee <clee@edx.org>
2016-03-29 16:32:49 -0400

    Setup screenshot tests

---
+++ VideoLocker/src/androidTest/java/org/edx/mobile/test/screenshot/LaunchScreenshotTests.java
@@ -0,0 +1,24 @@
+package org.edx.mobile.test.screenshot;
+
+import android.test.InstrumentationTestCase;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.facebook.testing.screenshot.Screenshot;
+import com.facebook.testing.screenshot.ViewHelpers;
+
+import org.edx.mobile.R;
+
+public class LaunchScreenshotTests extends InstrumentationTestCase {
+    public void test_takeScreenshotOf_launchScreen() throws Throwable {
+        LayoutInflater inflater = LayoutInflater.from(getInstrumentation().getTargetContext());
+        View view = inflater.inflate(R.layout.activity_launch, null, false);
+
+        ViewHelpers.setupView(view)
+                .setExactWidthDp(360)
+                .layout();
+
+        Screenshot.snap(view)
+                .record();
+    }
+}
