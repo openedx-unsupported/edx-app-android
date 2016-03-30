openedx-unsupported/edx-app-android

commit 688c155ceddf941c6e5dc27f238f19225d8c5bc9

Silently update threads list screen upon return
- SetThreadReadTask always called to ensure updating of recent activity.
- If threads are being sorted by recent activity, upon return the viewed
item is locally moved to the top before silent update.

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsSearchFragment.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.SearchView;
 
 import org.edx.mobile.R;
@@ -109,4 +110,14 @@ public class CourseDiscussionPostsSearchFragment extends CourseDiscussionPostsBa
             }
         });
     }
+
+    @Override
+    public void onRestart() {
+        nextPage = 1;
+        controller.resetSilently();
+    }
+
+    @Override
+    public void onItemClick(DiscussionThread thread, AdapterView<?> parent, View view,
+                            int position, long id) {}
 }

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsThreadFragment.java
@@ -60,6 +60,7 @@ public class CourseDiscussionPostsThreadFragment extends CourseDiscussionPostsBa
 
     private GetThreadListTask getThreadListTask;
     private int nextPage = 1;
+    private int mSelectedItem = -1;
 
     private enum EmptyQueryResultsFor {
         FOLLOWING,
@@ -169,6 +170,24 @@ public class CourseDiscussionPostsThreadFragment extends CourseDiscussionPostsBa
     public void onDestroy() {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onRestart() {
+        if (postsSort == DiscussionPostsSort.LAST_ACTIVITY_AT && mSelectedItem != -1) {
+            // Move the last viewed thread to the top
+            discussionPostsAdapter.moveToTop(mSelectedItem);
+            mSelectedItem = -1;
+        }
+
+        nextPage = 1;
+        controller.resetSilently();
+    }
+
+    @Override
+    public void onItemClick(DiscussionThread thread, AdapterView<?> parent, View view,
+                            int position, long id) {
+        mSelectedItem = position;
     }
 
     @SuppressWarnings("unused")

VideoLocker/src/main/java/org/edx/mobile/view/adapters/InfiniteScrollUtils.java
@@ -100,6 +100,7 @@ public class InfiniteScrollUtils {
 
     public interface InfiniteListController {
         void reset();
+        void resetSilently();
     }
 
     public static class PageLoadController<T> implements InfiniteListController {
@@ -123,14 +124,33 @@ public class InfiniteScrollUtils {
             }
         }
 
+        /**
+         * This function simply shows a spinner while loading the next page.
+         */
         private void onLoadMore() {
+            onLoadMore(false);
+        }
+
+        /**
+         * This function allows us to control the visibility of progress and lazily clear adapter
+         * after a page has loaded.
+         *
+         * @param isRefreshingSilently <code>true</code> If we're doing a silent refresh,
+         *                             <code>false</code> if pagination is being done as usual.
+         */
+        private void onLoadMore(final boolean isRefreshingSilently) {
             final int instanceLoadId = activeLoadId.get();
-            adapter.setProgressVisible(true);
+            if (!isRefreshingSilently) {
+                adapter.setProgressVisible(true);
+            }
             pageLoader.loadNextPage(new PageLoadCallback<T>() {
                 @Override
                 public void onPageLoaded(List<T> newItems, boolean hasMore) {
                     if (isAbandoned()) {
                         return;
+                    }
+                    if (isRefreshingSilently) {
+                        adapter.clear();
                     }
                     adapter.addAll(newItems);
                     hasMoreItems = hasMore;
@@ -154,11 +174,21 @@ public class InfiniteScrollUtils {
 
         @Override
         public void reset() {
-            activeLoadId.incrementAndGet(); // To disregard any in-progress loads
+            initLoading();
             adapter.clear();
+            onLoadMore();
+        }
+
+        @Override
+        public void resetSilently() {
+            initLoading();
+            onLoadMore(true);
+        }
+
+        private void initLoading() {
+            activeLoadId.incrementAndGet(); // To disregard any in-progress loads
             hasMoreItems = true;
             loading = true;
-            onLoadMore();
         }
     }
 

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsActivity.java
@@ -106,4 +106,9 @@ public class CourseDiscussionPostsActivity extends BaseSingleFragmentActivity  {
         }
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        ((CourseDiscussionPostsBaseFragment)getFirstFragment()).onRestart();
+    }
 }

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsBaseFragment.java
@@ -36,6 +36,18 @@ public abstract class CourseDiscussionPostsBaseFragment extends BaseFragment imp
 
     InfiniteScrollUtils.InfiniteListController controller;
 
+    /**
+     * Callback to match the restart behaviour with the parent activity.
+     */
+    public abstract void onRestart();
+
+    /**
+     * Extension for the child classes to add more functionality to the ListView's onItemClick
+     * function.
+     */
+    public abstract void onItemClick(DiscussionThread thread, AdapterView<?> parent,
+                                     View view, int position, long id);
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -46,13 +58,17 @@ public abstract class CourseDiscussionPostsBaseFragment extends BaseFragment imp
                 Context context = getContext();
                 DiscussionThread thread = discussionPostsAdapter.getItem(position);
                 router.showCourseDiscussionResponses(context, thread, courseData);
+
+                new SetThreadReadTask(context, thread, true).execute();
                 if (!thread.isRead()) {
-                    new SetThreadReadTask(context, thread, true).execute();
                     // Refresh the row to mark it as read immediately,
                     // pending the response from the server.
                     thread.setRead(true);
                     discussionPostsAdapter.getView(position, view, parent);
                 }
+
+                CourseDiscussionPostsBaseFragment.this.onItemClick(thread, parent, view,
+                        position, id);
             }
         });
     }

VideoLocker/src/main/java/org/edx/mobile/view/adapters/BaseListAdapter.java
@@ -125,6 +125,15 @@ public abstract class BaseListAdapter<T> extends ArrayAdapter<T> implements OnIt
         notifyDataSetChanged();
     }
 
+    public void moveToTop(int itemPosition) {
+        if (itemPosition < objects.size()) {
+            T item = objects.get(itemPosition);
+            objects.remove(itemPosition);
+            objects.add(0, item);
+            notifyDataSetChanged();
+        }
+    }
+
     /**
      * Clears all items from this adapter.
      */

commit 2838b35dfee635a2132ec53a07143bbbd040556f

Don't show error message on silent threads refresh
Commit 688c155ceddf941c6e5dc27f238f19225d8c5bc9 introduced silent
reloading of the discussion threads list upon Activity restart.
However, in spite of it being hidden from the user, the reloading logic
displayed an error message on failure, which is quite confusing for the
user. This commit patches the module to ensure that no error message
would be displayed to the user upon performing silent refreshes.

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsSearchFragment.java
@@ -97,7 +97,11 @@ public class CourseDiscussionPostsSearchFragment extends CourseDiscussionPostsBa
 
             @Override
             protected void onException(Exception ex) {
-                super.onException(ex);
+                // Don't display any error message if we're doing a silent
+                // refresh, as that would be confusing to the user.
+                if (!callback.isRefreshingSilently()) {
+                    super.onException(ex);
+                }
                 callback.onError();
                 nextPage = 1;
             }

VideoLocker/src/main/java/org/edx/mobile/view/CourseDiscussionPostsThreadFragment.java
@@ -280,7 +280,11 @@ public class CourseDiscussionPostsThreadFragment extends CourseDiscussionPostsBa
 
             @Override
             protected void onException(Exception ex) {
-                super.onException(ex);
+                // Don't display any error message if we're doing a silent
+                // refresh, as that would be confusing to the user.
+                if (!callback.isRefreshingSilently()) {
+                    super.onException(ex);
+                }
                 callback.onError();
                 nextPage = 1;
             }

VideoLocker/src/main/java/org/edx/mobile/view/adapters/InfiniteScrollUtils.java
@@ -101,6 +101,14 @@ public class InfiniteScrollUtils {
          * Callback for receiving an error during the page load.
          */
         public abstract void onError();
+
+        /**
+         * Returns the user visibility status of the page load.
+         *
+         * @return <code>true</code> If a silent refresh is being performed,
+         *         <code>false</code> if pagination is being done as usual.
+         */
+        public abstract boolean isRefreshingSilently();
     }
 
     public interface InfiniteListController {
@@ -173,6 +181,11 @@ public class InfiniteScrollUtils {
                     adapter.setProgressVisible(false);
                     hasMoreItems = false;
                     loading = false;
+                }
+
+                @Override
+                public boolean isRefreshingSilently() {
+                    return isRefreshingSilently;
                 }
 
                 /**