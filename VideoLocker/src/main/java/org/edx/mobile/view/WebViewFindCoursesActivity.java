9a3127a53d0df013d13e457cb01b455bf4daefaa
Michael Katz <katzman@gmail.com>
2016-04-12 11:02:51 -0400

    add the search bar

--- VideoLocker/src/main/java/org/edx/mobile/view/WebViewFindCoursesActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/WebViewFindCoursesActivity.java
@@ -1,7 +1,10 @@
 package org.edx.mobile.view;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
 import android.webkit.WebView;
+import android.support.v7.widget.SearchView;
 
 import org.edx.mobile.R;
 import org.edx.mobile.base.FindCoursesBaseActivity;
@@ -27,6 +30,36 @@ public class WebViewFindCoursesActivity extends FindCoursesBaseActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.loadUrl(environment.getConfig().getCourseDiscoveryConfig().getCourseSearchUrl());
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.find_courses, menu);
+        // Get the SearchView and set the searchable configuration
+        final SearchView searchView = (SearchView) menu.findItem(R.id.menu_item_search).getActionView();
+
+        Resources resources = getResources();
+        searchView.setQueryHint(resources.getString(R.string.search_for_courses));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String baseUrl = environment.getConfig().getCourseDiscoveryConfig().getCourseSearchUrl();
+                String searchUrl = baseUrl + "&search_query=" + query;
+                searchView.clearFocus();
+                webView.loadUrl(searchUrl);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return result;
+    }
+
 
     @Override
     protected void onOnline() {
