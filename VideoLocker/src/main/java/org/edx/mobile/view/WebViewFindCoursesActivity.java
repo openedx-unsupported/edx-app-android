2c69a5771faac0e301f6733dfc78330a9a89e643
Michael Katz <katzman@gmail.com>
2016-05-09 11:16:30 -0400

    update layout check

--- VideoLocker/src/main/java/org/edx/mobile/view/WebViewFindCoursesActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/WebViewFindCoursesActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -109,7 +108,7 @@ public class WebViewFindCoursesActivity extends FindCoursesBaseActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        if (mDrawerLayout.isDrawerVisible(Gravity.LEFT)) {
+        if (mDrawerLayout != null && mDrawerLayout.isDrawerVisible(GravityCompat.START)) {
             menu.clear();
         }
         return super.onPrepareOptionsMenu(menu);
