faec4d4414b88f2ad311821c35a839430452fc32
bguertin <bguertin@edx.org>
2016-09-09 14:03:31 -0400

    Automatically start activity and take screenshots in base test class

--- VideoLocker/src/androidTest/java/org/edx/mobile/test/screenshot/test/DiscoveryLaunchScreenshotTests.java
+++ VideoLocker/src/androidTest/java/org/edx/mobile/test/screenshot/test/DiscoveryLaunchScreenshotTests.java
@@ -1,8 +1,5 @@
 package org.edx.mobile.test.screenshot.test;
 
-import com.facebook.testing.screenshot.Screenshot;
-
-import org.edx.mobile.R;
 import org.edx.mobile.view.DiscoveryLaunchActivity;
 import org.edx.mobile.view.DiscoveryLaunchPresenter;
 import org.edx.mobile.view.PresenterActivityInstrumentationTest;
@@ -12,22 +9,16 @@ public class DiscoveryLaunchScreenshotTests extends PresenterActivityInstrumenta
 
     @Test
     public void testScreenshot_withCourseDiscoveryDisabled() {
-        startActivity(null);
         view.setEnabledButtons(false, false);
-        Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 
     @Test
     public void testScreenshot_withCourseDiscoveryEnabled() {
-        startActivity(null);
         view.setEnabledButtons(true, false);
-        Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 
     @Test
     public void testScreenshot_withCourseDiscoveryAndExploreSubjectsEnabled() {
-        startActivity(null);
         view.setEnabledButtons(true, true);
-        Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 }
