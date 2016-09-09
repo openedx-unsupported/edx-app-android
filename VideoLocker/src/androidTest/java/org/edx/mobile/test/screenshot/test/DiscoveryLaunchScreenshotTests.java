8992388cf0d5fe11a318852200d91c532d4f3c61
bguertin <bguertin@edx.org>
2016-09-09 11:45:26 -0400

    Simplify tests by automatically executing view methods on the application's UI thread

--- VideoLocker/src/androidTest/java/org/edx/mobile/test/screenshot/test/DiscoveryLaunchScreenshotTests.java
+++ VideoLocker/src/androidTest/java/org/edx/mobile/test/screenshot/test/DiscoveryLaunchScreenshotTests.java
@@ -1,55 +1,33 @@
 package org.edx.mobile.test.screenshot.test;
 
-import android.support.test.rule.UiThreadTestRule;
-import android.view.View;
-
 import com.facebook.testing.screenshot.Screenshot;
 
 import org.edx.mobile.R;
 import org.edx.mobile.view.DiscoveryLaunchActivity;
 import org.edx.mobile.view.DiscoveryLaunchPresenter;
 import org.edx.mobile.view.PresenterActivityInstrumentationTest;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class DiscoveryLaunchScreenshotTests extends PresenterActivityInstrumentationTest<DiscoveryLaunchActivity, DiscoveryLaunchPresenter, DiscoveryLaunchPresenter.ViewInterface> {
 
-    @Rule
-    public UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
-
     @Test
-    public void testScreenshot_withCourseDiscoveryDisabled() throws Throwable {
+    public void testScreenshot_withCourseDiscoveryDisabled() {
         startActivity(null);
-        uiThreadTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.setEnabledButtons(false, false);
-            }
-        });
+        view.setEnabledButtons(false, false);
         Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 
     @Test
-    public void testScreenshot_withCourseDiscoveryEnabled() throws Throwable {
+    public void testScreenshot_withCourseDiscoveryEnabled() {
         startActivity(null);
-        uiThreadTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.setEnabledButtons(true, false);
-            }
-        });
+        view.setEnabledButtons(true, false);
         Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 
     @Test
-    public void testScreenshot_withCourseDiscoveryAndExploreSubjectsEnabled() throws Throwable {
+    public void testScreenshot_withCourseDiscoveryAndExploreSubjectsEnabled() {
         startActivity(null);
-        uiThreadTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.setEnabledButtons(true, true);
-            }
-        });
+        view.setEnabledButtons(true, true);
         Screenshot.snap(activity.findViewById(R.id.root_view)).record();
     }
 }
--- VideoLocker/src/androidTest/java/org/edx/mobile/view/PresenterActivityInstrumentationTest.java
+++ VideoLocker/src/androidTest/java/org/edx/mobile/view/PresenterActivityInstrumentationTest.java
@@ -1,15 +1,23 @@
 package org.edx.mobile.view;
 
 import android.content.Intent;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.edx.mobile.loader.AsyncTaskResult;
 import org.edx.mobile.test.EdxInstrumentationTestApplication;
 import org.edx.mobile.test.GenericSuperclassUtils;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import static org.mockito.Mockito.mock;
 
@@ -23,11 +31,20 @@ public abstract class PresenterActivityInstrumentationTest<ActivityT extends Pre
     @Rule
     public ActivityTestRule<ActivityT> mActivityRule = new ActivityTestRule<>(getActivityType(), true, false);
 
+    @Rule
+    public UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
+
     protected void startActivity(@Nullable final Intent intent) {
         this.presenter = mock(getPresenterType());
         ((EdxInstrumentationTestApplication) InstrumentationRegistry.getTargetContext().getApplicationContext()).setNextPresenter(presenter);
         this.activity = mActivityRule.launchActivity(intent);
-        this.view = activity.view;
+        // To simplify tests, we automatically execute view methods on the application's UI thread.
+        this.view = UiThreadInvocationHandler.newProxyInstance(uiThreadTestRule, activity.view, getViewType());
+    }
+
+    @SuppressWarnings("unchecked")
+    private Class<ActivityT> getActivityType() {
+        return (Class<ActivityT>) GenericSuperclassUtils.getTypeArguments(getClass(), PresenterActivityInstrumentationTest.class)[0];
     }
 
     @SuppressWarnings("unchecked")
@@ -36,7 +53,52 @@ public abstract class PresenterActivityInstrumentationTest<ActivityT extends Pre
     }
 
     @SuppressWarnings("unchecked")
-    private Class<ActivityT> getActivityType() {
-        return (Class<ActivityT>) GenericSuperclassUtils.getTypeArguments(getClass(), PresenterActivityInstrumentationTest.class)[0];
+    private Class<ViewT> getViewType() {
+        return (Class<ViewT>) GenericSuperclassUtils.getTypeArguments(getClass(), PresenterActivityInstrumentationTest.class)[2];
+    }
+
+    public static class UiThreadInvocationHandler<ViewT> implements InvocationHandler {
+
+        @NonNull
+        private final UiThreadTestRule uiThreadTestRule;
+
+        @NonNull
+        private final ViewT impl;
+
+        public UiThreadInvocationHandler(@NonNull UiThreadTestRule uiThreadTestRule, @NonNull ViewT impl) {
+            this.uiThreadTestRule = uiThreadTestRule;
+            this.impl = impl;
+        }
+
+        @Override
+        public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+            final AsyncTaskResult<Object> result = new AsyncTaskResult<>();
+            final Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        result.setResult(method.invoke(impl, args));
+                    } catch (Exception e) {
+                        result.setEx(e);
+                    }
+                }
+            };
+            if (Looper.myLooper() == Looper.getMainLooper()) {
+                // Already on UI thread
+                runnable.run();
+            } else {
+                uiThreadTestRule.runOnUiThread(runnable);
+            }
+            if (null != result.getEx()) {
+                throw result.getEx();
+            }
+            return result.getResult();
+        }
+
+        @SuppressWarnings("unchecked")
+        @NonNull
+        public static <ViewT> ViewT newProxyInstance(@NonNull UiThreadTestRule uiThreadTestRule, @NonNull ViewT view, @NonNull Class<ViewT> viewType) {
+            return (ViewT) Proxy.newProxyInstance(viewType.getClassLoader(), new Class<?>[]{viewType}, new UiThreadInvocationHandler(uiThreadTestRule, view));
+        }
     }
 }
