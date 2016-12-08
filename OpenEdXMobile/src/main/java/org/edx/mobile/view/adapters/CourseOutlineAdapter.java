4ed245bda45ac8de0a1b06de5da3c3ce8a584e38
christopher lee <clee@edx.org>
2016-12-08 17:11:30 -0500

    Update download icon

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/adapters/CourseOutlineAdapter.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/adapters/CourseOutlineAdapter.java
@@ -426,7 +426,7 @@ public class CourseOutlineAdapter extends BaseAdapter {
                 row.bulkDownload.setIconColorResource(R.color.edx_brand_gray_accent);
                 break;
             case ONLINE:
-                row.bulkDownload.setIcon(FontAwesomeIcons.icon_download);
+                row.bulkDownload.setIcon(FontAwesomeIcons.fa_download);
                 row.bulkDownload.setIconAnimation(Animation.NONE);
                 row.bulkDownload.setIconColorResource(R.color.edx_brand_gray_accent);
                 break;
