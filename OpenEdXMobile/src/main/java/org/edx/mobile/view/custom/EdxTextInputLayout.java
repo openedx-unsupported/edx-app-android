ec61acd8b483b6c3060887593039112034094edc
Mian Khalid <mkhalid@edx.org>
2017-02-14 17:22:38 +0500

    TextInputLayout’s hint made accessible in activated state
    - EdxTextInputLayout (subclass of TextInputLayout) sets the hint’s text
    appearance by default to an accessible color.
    - Usages of TextInputLayout replaced with EdxTextInputLayout throughout
    the project.

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/EdxTextInputLayout.java
@@ -0,0 +1,30 @@
+package org.edx.mobile.view.custom;
+
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
+import android.util.AttributeSet;
+
+import org.edx.mobile.R;
+
+public class EdxTextInputLayout extends TextInputLayout {
+    private final int HINT_TEXT_APPEARANCE_STYLE = R.style.edX_Widget_TextInputLayout_HintTextAppearance;
+
+    public EdxTextInputLayout(Context context) {
+        super(context);
+        setCustomHintAppearance();
+    }
+
+    public EdxTextInputLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomHintAppearance();
+    }
+
+    public EdxTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setCustomHintAppearance();
+    }
+
+    protected void setCustomHintAppearance() {
+        setHintTextAppearance(HINT_TEXT_APPEARANCE_STYLE);
+    }
+}
