0df88375a296457d4dd5a15fdc4761ee49c0f247
renevatium <rene@proversity.org>
2017-04-03 14:47:23 +0200

    respect dash in email domain names

--- OpenEdXMobile/src/main/java/org/edx/mobile/util/InputValidationUtil.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/util/InputValidationUtil.java
@@ -10,7 +10,7 @@ public class InputValidationUtil {
             return false;
 
         Pattern p = Pattern
-                .compile("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+                .compile("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         // Match the given string with the pattern
         Matcher m = p.matcher(text);
         // check whether match is found
