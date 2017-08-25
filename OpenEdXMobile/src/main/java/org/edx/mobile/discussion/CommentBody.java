514810c1faa56d854922aa904bd2f738bf145255
Mian Khalid <mkhalid@edx.org>
2017-08-25 18:50:38 +0500

    Change Content-Type of create comment/response API
    - LEARNER-2734
    - Changes done to support the Content-Type: application/json instead of
    the previously used application/x-www-form-urlencoded type

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/discussion/CommentBody.java
@@ -0,0 +1,17 @@
+package org.edx.mobile.discussion;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class CommentBody {
+    private String threadId;
+    private String rawBody;
+    private String parentId;
+
+    public CommentBody(@NonNull String threadId, @NonNull String rawBody, @Nullable String parentId) {
+        this.threadId = threadId;
+        this.rawBody = rawBody;
+        this.parentId = parentId;
+    }
+}
