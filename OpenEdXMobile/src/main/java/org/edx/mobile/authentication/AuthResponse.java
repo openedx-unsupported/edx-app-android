029b8aea53fa05fa4f7bb7dd5fb129cf614de23e
Mian Khalid <mkhalid@edx.org>
2017-10-30 11:09:47 +0500

    Test travis lint

--- OpenEdXMobile/src/main/java/org/edx/mobile/authentication/AuthResponse.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/authentication/AuthResponse.java
@@ -7,33 +7,33 @@ import java.io.Serializable;
 
 /**
  * This class represents response of Authentication call to server.
- * 
+ *
  */
 public class AuthResponse implements Serializable {
 
     private static final String ERROR_INVALID_GRANT = "invalid_grant";
-    
+
     public String access_token;
     public String token_type;
     public long expires_in;
     public String scope;
     public String error;
     public String refresh_token;
-    
+
     // non-api field
     public ProfileModel profile;
-    
+
     @Override
     public String toString() {
         return String.format(
                 "access_token=%s; token_type=%s; expires_in=%d; scope=%s; refresh_token=%s",
                 access_token, token_type, expires_in, scope, refresh_token);
     }
-    
+
     public boolean isSuccess() {
         return (error == null && access_token != null);
     }
-    
+
     /**
      * Returns true if the response indicates that account is inactive.
      * @return
