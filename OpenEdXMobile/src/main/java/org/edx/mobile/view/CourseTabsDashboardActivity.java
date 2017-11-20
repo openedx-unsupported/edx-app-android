3141cba9e6e818f82186383d269f208670d437b2
Mian Khalid <mkhalid@edx.org>
2017-11-20 18:09:51 +0500

    Block drawer from opening on tabs dashboard screen

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/CourseTabsDashboardActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/CourseTabsDashboardActivity.java
@@ -2,6 +2,7 @@ package org.edx.mobile.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
@@ -20,6 +21,12 @@ public class CourseTabsDashboardActivity extends BaseSingleFragmentActivity {
         intent.putExtra(EXTRA_ANNOUNCEMENTS, announcements);
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        blockDrawerFromOpening();
     }
 
     @Override
