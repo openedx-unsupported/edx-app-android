f0e73b47600c65606f3dc3929e2018e2e9c2d9be
M Zohaib Asaad <zohaib.asaad@gmail.com>
2018-01-22 13:37:08 +0500

    Minor fix in youtube fragment transaction

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/adapters/CourseCardViewHolder.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/adapters/CourseCardViewHolder.java
@@ -5,16 +5,11 @@ import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import org.edx.mobile.R;
 import org.edx.mobile.model.api.CourseEntry;
-import org.edx.mobile.util.images.TopAnchorFillWidthTransformation;
 
 public class CourseCardViewHolder extends BaseListAdapter.BaseViewHolder {
 
