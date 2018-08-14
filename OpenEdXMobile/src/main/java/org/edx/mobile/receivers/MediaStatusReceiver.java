47610526130463e370f5813ea9207e7cbb5446ea
Albert St. Aubin <astaubin@edx.org>
2018-08-14 11:05:50 -0400

    Rebase and update with current testing and PR comments.

--- OpenEdXMobile/src/main/java/org/edx/mobile/receivers/MediaStatusReceiver.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/receivers/MediaStatusReceiver.java
@@ -38,62 +38,79 @@ public class MediaStatusReceiver extends RoboBroadcastReceiver {
     public void handleReceive(Context context, Intent intent) {
         String username = loginPrefs.getUsername();
         String hashedUsername = (username != null) ? Sha1Util.SHA1(username) : null;
+
+        // TODO: Is this Path a good method for Identifying the path in the DB
+        // should we updated the model to have an SD Card Flag?
         final String sdCardPath = intent.getDataString().replace("file://", "");
 
-        switch (intent.getAction()) {
-            case Intent.ACTION_MEDIA_REMOVED:
-            case Intent.ACTION_MEDIA_UNMOUNTED:
-                final PrefManager prefManager =
-                        new PrefManager(context, PrefManager.Pref.SD_CARD);
-                prefManager.put(PrefManager.Key.DOWNLOAD_TO_SDCARD, false);
-                db.getAllVideos(hashedUsername, new DataCallback<List<VideoModel>>() {
-                    @Override
-                    public void onResult(List<VideoModel> result) {
-                        for (VideoModel videoModel : result) {
-                            String videoPath = videoModel.getFilePath();
-                            updateVideoDownloadState(
-                                    videoModel,
-                                    DownloadEntry.DownloadedState.ONLINE.ordinal(),
-                                    sdCardPath,
-                                    videoPath
-                            );
-                        }
-                    }
-                    @Override
-                    public void onFail(Exception ex) {
-                        //TODO Proper error handling here?
-                        Log.e(this.getClass().getSimpleName(),
-                                "Unable to get to get list of Videos"
-                        );
-                    }
-                });
-                break;
-            case Intent.ACTION_MEDIA_MOUNTED:
-                db.getAllVideos(hashedUsername, new DataCallback<List<VideoModel>>() {
-                    @Override
-                    public void onResult(List<VideoModel> result) {
-                        for (VideoModel videoModel : result) {
-                            String videoPath = videoModel.getFilePath();
-                            File file = new File(videoPath);
-                            if (file.exists()) {
-                                updateVideoDownloadState(
-                                        videoModel,
-                                        DownloadEntry.DownloadedState.DOWNLOADED.ordinal(),
-                                        sdCardPath,
-                                        videoPath
-                                );
-                            }
-                        }
-                    }
-                    @Override
-                    public void onFail(Exception ex) {
-                        Log.e(this.getClass().getSimpleName(),
-                                "Unable to get to get list of Videos"
-                        );
-                    }
-                });
-                break;
+        String action = intent.getAction();
+        if(action != null) {
+            switch (action) {
+                case Intent.ACTION_MEDIA_REMOVED:
+                case Intent.ACTION_MEDIA_UNMOUNTED:
+                    handleSDCardUnmounted(context, hashedUsername, sdCardPath);
+                    break;
+                case Intent.ACTION_MEDIA_MOUNTED:
+                    handleSDCardMounted(hashedUsername, sdCardPath);
+                    break;
+            }
         }
+    }
+
+    private void handleSDCardUnmounted(Context context,
+                                       String hashedUsername,
+                                       final String sdCardPath){
+        final PrefManager prefManager =
+                new PrefManager(context, PrefManager.Pref.SD_CARD);
+        prefManager.put(PrefManager.Key.DOWNLOAD_TO_SDCARD, false);
+        db.getAllVideos(hashedUsername, new DataCallback<List<VideoModel>>() {
+            @Override
+            public void onResult(List<VideoModel> result) {
+                for (VideoModel videoModel : result) {
+                    String videoPath = videoModel.getFilePath();
+                    updateVideoDownloadState(
+                            videoModel,
+                            DownloadEntry.DownloadedState.ONLINE.ordinal(),
+                            sdCardPath,
+                            videoPath
+                    );
+                }
+            }
+
+            @Override
+            public void onFail(Exception ex) {
+                Log.e(this.getClass().getSimpleName(),
+                        "Unable to get to get list of Videos"
+                );
+            }
+        });
+    }
+
+    private void handleSDCardMounted(String hashedUsername, final String sdCardPath){
+        db.getAllVideos(hashedUsername, new DataCallback<List<VideoModel>>() {
+            @Override
+            public void onResult(List<VideoModel> result) {
+                for (VideoModel videoModel : result) {
+                    String videoPath = videoModel.getFilePath();
+                    File file = new File(videoPath);
+                    if (file.exists()) {
+                        updateVideoDownloadState(
+                                videoModel,
+                                DownloadEntry.DownloadedState.DOWNLOADED.ordinal(),
+                                sdCardPath,
+                                videoPath
+                        );
+                    }
+                }
+            }
+
+            @Override
+            public void onFail(Exception ex) {
+                Log.e(this.getClass().getSimpleName(),
+                        "Unable to get to get list of Videos"
+                );
+            }
+        });
     }
 
     public void updateVideoDownloadState(VideoModel videoModel,
@@ -101,14 +118,16 @@ public class MediaStatusReceiver extends RoboBroadcastReceiver {
                                          String sdCardPath,
                                          String videoPath) {
         if (videoPath.contains(sdCardPath)) {
-            // TODO: Check that the video files are still available.
-            NativeDownloadModel dm = new NativeDownloadModel();
-            dm.dmid = videoModel.getDmId();
-            dm.filepath = videoModel.getFilePath();
-            dm.size = videoModel.getSize();
-            dm.downloaded = downloadState;
-            videoModel.setDownloadInfo(dm);
-            db.updateDownloadingVideoInfoByVideoId(videoModel, null);
+            File testFileExists = new File(videoPath);
+            if (testFileExists.exists()) {
+                NativeDownloadModel dm = new NativeDownloadModel();
+                dm.dmid = videoModel.getDmId();
+                dm.filepath = videoModel.getFilePath();
+                dm.size = videoModel.getSize();
+                dm.downloaded = downloadState;
+                videoModel.setDownloadInfo(dm);
+                db.updateDownloadingVideoInfoByVideoId(videoModel, null);
+            }
         }
     }
 }
