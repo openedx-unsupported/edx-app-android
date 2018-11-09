3b56d1b26f2bbacdd9ff11b38814d7a6914aeae0
Mian Khalid <mkhalid@edx.org>
2018-11-09 18:54:59 +0500

    Fix TransactionTooLargeException on unit navigation
    - LEARNER-6680
    TransactionTooLargeException happens when an Activity/Fragment is in the
    process of stopping, that means that the Activity/Fragment is trying to
    send its saved state Bundles to the system OS for safe keeping for
    restoration later (after a config change or process death) but that one
    or more of the Bundles it sent is too large (i.e. greater than 1MB).
    To avoid this scenario on CourseUnitNavigationActivity, CourseComponent
    object is now saved on the disk using the Bridge library which allows
    writing and retrieving it from the disk.

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/CourseUnitFragment.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/CourseUnitFragment.java
@@ -1,15 +1,17 @@
 package org.edx.mobile.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
+import com.evernote.android.state.State;
 import com.google.inject.Inject;
+import com.livefront.bridge.Bridge;
 
+import org.edx.mobile.base.BaseFragment;
 import org.edx.mobile.core.IEdxEnvironment;
 import org.edx.mobile.model.course.CourseComponent;
 import org.edx.mobile.view.common.PageViewStateCallback;
 import org.edx.mobile.view.common.RunnableCourseComponent;
-
-import org.edx.mobile.base.BaseFragment;
 
 public abstract class CourseUnitFragment extends BaseFragment implements PageViewStateCallback, RunnableCourseComponent {
     public interface HasComponent {
@@ -18,6 +20,7 @@ public abstract class CourseUnitFragment extends BaseFragment implements PageVie
         void navigatePreviousComponent();
     }
 
+    @State
     protected CourseComponent unit;
     protected HasComponent hasComponentCallback;
 
@@ -27,8 +30,36 @@ public abstract class CourseUnitFragment extends BaseFragment implements PageVie
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        unit = getArguments() == null ? null :
-                (CourseComponent) getArguments().getSerializable(Router.EXTRA_COURSE_UNIT);
+        if (getArguments() != null) {
+            final Bundle args = getArguments();
+            unit = (CourseComponent) getArguments().getSerializable(Router.EXTRA_COURSE_UNIT);
+            if (unit != null) {
+                /*
+                The size of `unit` object could be very big in some courses that have lots of
+                sections/subsections/units in them. So, we need to ensure that this object isn't
+                stored in the fragment's extras otherwise we might encounter
+                TransactionTooLargeException.
+                 */
+                args.putSerializable(Router.EXTRA_COURSE_UNIT, null);
+                setArguments(args);
+            }
+        }
+        /*
+        To retain the `unit` object during fragment recreation, we're relying on the Bridge library
+        which'll write the object to disk and allow us to restore while the fragment is being
+        recreated. Consequently, avoiding the TransactionTooLargeException.
+        More info:
+        - https://medium.com/@mdmasudparvez/android-os-transactiontoolargeexception-on-nougat-solved-3b6e30597345
+        - https://github.com/livefront/bridge
+        - https://openedx.atlassian.net/browse/LEARNER-6680
+        */
+        Bridge.restoreInstanceState(this, savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bridge.saveInstanceState(this, outState);
     }
 
     @Override
