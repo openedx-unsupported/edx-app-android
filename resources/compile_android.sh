41b771b5ab409d7bd30631b8d54456e44e0f7f42
Naveed <nsaleem@edx.org>
2018-11-30 16:58:26 +0500

    LEARNER-6123 Test - Jenkins - check existing implementation to build an AUT(application Under Test) image
    - Add a Jenkinsfile to impelment Jenkins Pipelines for Advance Continuous Integration
    - Add compile_android shell file which will create properties file at repo , add required parameter and compile app android code
    - Add validate_build shell which will validate create apk

---
+++ resources/compile_android.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clear old outputs if any 
+rm -rf OpenEdXMobile/build/
+
+# Compile app 
+echo 'Compiling Android App'
+./gradlew printConfig assembleProdDebuggable 
