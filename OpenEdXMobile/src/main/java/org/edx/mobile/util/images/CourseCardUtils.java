ee9b332b3c96b716f55f9495c56177d06fcef01a
Mian Khalid <mkhalid@edx.org>
2018-12-03 19:06:43 +0500

    Implement content gating on Course Listing
    - LEARNER-6639
    - Courseware is now hidden from user if the audit access expiry date
    has expired which now comes in the v1 of Enrollments API
    - Add final keyword wherever necessary in CourseCardUtils
    - Provide today's date explicitly for ease in tests
    - Update mock api response for Enrollments API

--- OpenEdXMobile/src/main/java/org/edx/mobile/util/images/CourseCardUtils.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/util/images/CourseCardUtils.java
@@ -2,11 +2,14 @@ package org.edx.mobile.util.images;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.text.format.DateUtils;
 
 import org.edx.mobile.R;
 import org.edx.mobile.course.CourseDetail;
 import org.edx.mobile.model.api.CourseEntry;
+import org.edx.mobile.model.api.EnrolledCoursesResponse;
 import org.edx.mobile.model.api.StartType;
 import org.edx.mobile.util.DateUtil;
 import org.edx.mobile.util.ResourceUtil;
@@ -17,43 +20,93 @@ import java.util.List;
 
 public enum CourseCardUtils {
     ;
+    public static final long SEVEN_DAYS_IN_MILLIS = 604800000L;
 
-    public static boolean isStarted(String start) {
-        // check if "start" date has passed
-        if (start == null)
-            return false;
-
-        Date startDate = DateUtil.convertToDate(start);
-        Date today = new Date();
-        return today.after(startDate);
-    }
-
-    public static boolean isEnded(String end) {
-        // check if "end" date has passed
-        if (end == null)
-            return false;
-
-        Date endDate = DateUtil.convertToDate(end);
-        Date today = new Date();
-        return today.after(endDate);
+    /**
+     * Checks if the given date is past today.
+     *
+     * @param today     Today's date.
+     * @param otherDate Other date to cross-match with today's date.
+     * @return <code>true</code> if the other date is past today,
+     * <code>false</code> otherwise.
+     */
+    public static boolean isDatePassed(@NonNull Date today, @Nullable String otherDate) {
+        return otherDate != null && today.after(DateUtil.convertToDate(otherDate));
     }
 
     public static String getFormattedDate(Context context, String start, String end, StartType start_type, String start_display) {
-        CharSequence formattedDate;
-        if (isStarted(start)) {
-            Date endDate = DateUtil.convertToDate(end);
-            if (endDate == null) {
-                return null;
-            } else if (isEnded(end)) {
-                formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
-                        .label_ended, "date", DateUtil.formatDateWithNoYear(endDate.getTime()));
+        return getFormattedDate(context, new Date(), null, start, end, start_type, start_display);
+    }
+
+    /**
+     * Provides a formatted date string taking various dates related to a course in account like
+     * start date, end date and expiry date.
+     * <br>Example outputs:
+     * <br>Starting January 15
+     * <br>Course access expires in 4 days
+     * <br>Course access expired on February 15
+     * <br>Ending February 15
+     *
+     * @param context       The context.
+     * @param today         Today's date.
+     * @param expiry        Expiry date.
+     * @param start         Start date.
+     * @param end           End date.
+     * @param start_type    Tell's if a course's start date is formatted, unformatted or unset.
+     * @param start_display Tell's how the server represents the start date based on start_type.
+     * @return A formatted date.
+     */
+    public static String getFormattedDate(Context context, Date today, String expiry, String start, String end, StartType start_type, String start_display) {
+        final CharSequence formattedDate;
+        // check if "start" date has passed
+        if (isDatePassed(today, start)) {
+            if (expiry != null) {
+                final Date expiryDate = DateUtil.convertToDate(expiry);
+                final long dayDifferenceInMillies;
+                if (today.after(expiryDate)) {
+                    dayDifferenceInMillies = today.getTime() - expiryDate.getTime();
+                } else {
+                    dayDifferenceInMillies = expiryDate.getTime() - today.getTime();
+                }
+
+                // check if "expiry" date has passed
+                if (isDatePassed(today, expiry)) {
+                    if (dayDifferenceInMillies > SEVEN_DAYS_IN_MILLIS) {
+                        formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                                .label_expired_on, "date", DateUtil.formatDateWithNoYear(expiryDate.getTime()));
+                    } else {
+                        final CharSequence timeSpan = DateUtils.getRelativeTimeSpanString(expiryDate.getTime(), today.getTime(),
+                                DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString().toLowerCase();
+                        formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                                .label_expired, "date", timeSpan);
+                    }
+                } else {
+                    if (dayDifferenceInMillies > SEVEN_DAYS_IN_MILLIS) {
+                        formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                                .label_expires_on, "date", DateUtil.formatDateWithNoYear(expiryDate.getTime()));
+                    } else {
+                        final CharSequence timeSpan = DateUtils.getRelativeTimeSpanString(expiryDate.getTime(), today.getTime(),
+                                DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString().toLowerCase();
+                        formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                                .label_expires, "date", timeSpan);
+                    }
+                }
             } else {
-                formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
-                        .label_ending, "date", DateUtil.formatDateWithNoYear(endDate.getTime()));
+                final Date endDate = DateUtil.convertToDate(end);
+                if (endDate == null) {
+                    return null;
+                } else if (isDatePassed(today, end)) {
+                    // if "end" date has passed
+                    formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                            .label_ended, "date", DateUtil.formatDateWithNoYear(endDate.getTime()));
+                } else {
+                    formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
+                            .label_ending, "date", DateUtil.formatDateWithNoYear(endDate.getTime()));
+                }
             }
         } else {
             if (start_type == StartType.TIMESTAMP && !TextUtils.isEmpty(start)) {
-                Date startDate = DateUtil.convertToDate(start);
+                final Date startDate = DateUtil.convertToDate(start);
                 formattedDate = ResourceUtil.getFormattedString(context.getResources(), R.string
                         .label_starting, "date", DateUtil.formatDateWithNoYear(startDate.getTime()));
             } else if (start_type == StartType.STRING && !TextUtils.isEmpty(start_display)) {
@@ -67,6 +120,18 @@ public enum CourseCardUtils {
         }
 
         return formattedDate.toString();
+    }
+
+    public static String getFormattedDate(@NonNull Context context, @NonNull EnrolledCoursesResponse enrolledCourse) {
+        final CourseEntry course = enrolledCourse.getCourse();
+        return CourseCardUtils.getFormattedDate(
+                context,
+                new Date(),
+                enrolledCourse.getAuditAccessExpires(),
+                course.getStart(),
+                course.getEnd(),
+                course.getStartType(),
+                course.getStartDisplay());
     }
 
     public static String getFormattedDate(@NonNull Context context, @NonNull CourseEntry course) {
