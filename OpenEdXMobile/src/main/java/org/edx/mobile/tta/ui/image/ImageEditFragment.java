24d738cf03ab888b83bc9f96b0e80de261b78549
Ayushi <ayushi.purbey@gmail.com>
2019-03-12 19:16:38 +0530

    bottom sheet attached

--- OpenEdXMobile/src/main/java/org/edx/mobile/tta/ui/image/ImageEditFragment.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/tta/ui/image/ImageEditFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import org.edx.mobile.R;
 import org.edx.mobile.tta.ui.base.TaBaseFragment;
 import org.edx.mobile.tta.ui.image.view_model.ImageEditModule;
@@ -14,6 +17,7 @@ import org.edx.mobile.tta.ui.image.view_model.ImageEditModule;
 public class ImageEditFragment extends TaBaseFragment {
     public static final String TAG = ImageEditFragment.class.getCanonicalName();
     private ImageEditModule viewModel;
+    private Spinner spinner_classes;
 
 
     @Override
@@ -32,6 +36,13 @@ public class ImageEditFragment extends TaBaseFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = binding(inflater, container, R.layout.fragment_edit_user_image, viewModel)
                 .getRoot();
+        spinner_classes =  view.findViewById(R.id.spinner_classes);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.spinner_classes, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner_classes.setAdapter(adapter);
 
         return view;
     }
