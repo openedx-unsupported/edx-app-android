0be51da0e4177f7c0eb96fbb6a5a24e1af6ff38a
Mian Khalid <mkhalid@edx.org>
2019-03-13 15:39:23 +0500

    Add missed PROGRAM entry in ScreenDef

--- OpenEdXMobile/src/main/java/org/edx/mobile/deeplink/ScreenDef.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/deeplink/ScreenDef.java
@@ -8,12 +8,13 @@ import java.lang.annotation.RetentionPolicy;
 import static org.edx.mobile.deeplink.Screen.COURSE_DASHBOARD;
 import static org.edx.mobile.deeplink.Screen.COURSE_VIDEOS;
 import static org.edx.mobile.deeplink.Screen.PROFILE;
+import static org.edx.mobile.deeplink.Screen.PROGRAM;
 
 /**
  * Denotes that a String parameter, field or method return value is expected
  * to be a String reference (e.g. {@link Screen#COURSE_DASHBOARD}).
  */
 @Retention(RetentionPolicy.SOURCE)
-@StringDef({COURSE_DASHBOARD, COURSE_VIDEOS, PROFILE})
+@StringDef({COURSE_DASHBOARD, COURSE_VIDEOS, PROGRAM, PROFILE})
 public @interface ScreenDef {
 }
