90bb342c29c163750450f1f8737a2415bd0e90e8
Naveed <nsaleem@edx.org>
2019-03-21 15:39:47 +0500

    Removed unnecessary emulator related code because we will be moving towards execution on Amazaon Device Form

--- resources/setup_emulator.sh
+++ resources/setup_emulator.sh
@@ -3,48 +3,12 @@
 set -e
 
 EXPECTED_DEVICE_NAME="emulator-5554"
-BOOT_FINISHED_STATUS="stopped"
-BOOT_CURRENT_STATUS=""
-SYS_BOOT_CURRENT_STATUS=""
-SYS_BOOT_FINISHED_STATUS="1"
-ATTEMPT=0
-TIMEOUT=300
 
-kill_all_emus() {
-    for emu_device in $($ANDROID_HOME/platform-tools/adb devices -l |grep 'device product:' |cut -d' ' -f1); do
-        echo "Killing emulator: $emu_device"
-        adb -s $emu_device emu kill
-    done
-}
+# Connect Amazon Device Form 
+# This part will be WIP
 
-# Kill all existing emulators if any
-kill_all_emus
 
-echo 'Creating emulator'
-echo 'n' | $ANDROID_HOME/tools/bin/avdmanager create avd -n test-android -f -k "system-images;android-27;google_apis;x86"
-sleep 5
-
-echo 'loading emulator'
-$ANDROID_HOME/emulator/emulator -avd test-android -no-boot-anim -no-window -noaudio & 
-while true; do
-    BOOT_CURRENT_STATUS=`$ANDROID_HOME/platform-tools/adb -e shell getprop init.svc.bootanim &`
-    SYS_BOOT_CURRENT_STATUS=`$ANDROID_HOME/platform-tools/adb shell getprop sys.boot_completed &`    
-    if [ "$BOOT_CURRENT_STATUS" = "$BOOT_FINISHED_STATUS" ] && [ "$SYS_BOOT_CURRENT_STATUS" = "$SYS_BOOT_FINISHED_STATUS" ]; 
-    then 
-        echo "emulator is ready"
-        break
-    else 
-        let "ATTEMPT += 1"
-        echo "$ATTEMPT sec, waiting more for emulator to start. $BOOT_CURRENT_STATUS & $SYS_BOOT_STATUS"
-        if [ $ATTEMPT -gt $TIMEOUT ]; 
-        then
-            echo "Timeout ($TIMEOUT seconds) reached; failed to start emulator. $BOOT_CURRENT_STATUS & $SYS_BOOT_STATUS"
-            exit 1
-        fi
-        sleep 1
-    fi    
-done
-
+# Verify if device is available 
 $ANDROID_HOME/platform-tools/adb devices |grep $EXPECTED_DEVICE_NAME
 if [ $? == 0 ]; 
 then
