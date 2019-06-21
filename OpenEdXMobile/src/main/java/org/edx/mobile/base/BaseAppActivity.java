254115a892e7333caedb67bef6b934287e84c585
Farhan Khan <mfarhan133@gmail.com>
2019-06-21 18:10:13 +0500

    Add introductory overlay

--- OpenEdXMobile/src/main/java/org/edx/mobile/base/BaseAppActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/base/BaseAppActivity.java
@@ -2,9 +2,15 @@ package org.edx.mobile.base;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastState;
+import com.google.android.gms.cast.framework.CastStateListener;
+import com.google.android.gms.cast.framework.IntroductoryOverlay;
 
 import org.edx.mobile.R;
 import org.edx.mobile.event.NewRelicEvent;
@@ -13,6 +19,11 @@ import de.greenrobot.event.EventBus;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public abstract class BaseAppActivity extends RoboAppCompatActivity {
+    private MenuItem mediaRouteMenuItem;
+    private IntroductoryOverlay mIntroductoryOverlay;
+    private CastStateListener castStateListener;
+    private CastContext castContext;
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -22,13 +33,65 @@ public abstract class BaseAppActivity extends RoboAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EventBus.getDefault().post(new NewRelicEvent(getClass().getSimpleName()));
+
+        castContext = CastContext.getSharedInstance(this);
+        castStateListener = new CastStateListener() {
+            @Override
+            public void onCastStateChanged(int newState) {
+                if (newState != CastState.NO_DEVICES_AVAILABLE) {
+                    showIntroductoryOverlay();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        if (castContext != null) {
+            castContext.addCastStateListener(castStateListener);
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (castContext != null) {
+            castContext.removeCastStateListener(castStateListener);
+        }
+        super.onPause();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.player_fragment_menu, menu);
-        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu, R.id.media_route_menu_item);
+        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu, R.id.media_route_menu_item);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void showIntroductoryOverlay() {
+        if (mIntroductoryOverlay != null) {
+            mIntroductoryOverlay.remove();
+        }
+        if ((mediaRouteMenuItem != null) && mediaRouteMenuItem.isVisible()) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    mIntroductoryOverlay = new IntroductoryOverlay.Builder(
+                            BaseAppActivity.this, mediaRouteMenuItem)
+                            .setTitleText("Introducing Cast")
+                            .setSingleTime()
+                            .setOnOverlayDismissedListener(
+                                    new IntroductoryOverlay.OnOverlayDismissedListener() {
+                                        @Override
+                                        public void onOverlayDismissed() {
+                                            mIntroductoryOverlay = null;
+                                        }
+                                    })
+                            .build();
+                    mIntroductoryOverlay.show();
+                }
+            });
+        }
     }
 }
