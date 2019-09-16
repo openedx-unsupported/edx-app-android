541ac26ba3f2aef50cbe51e8e620a006274cc28a
ankit <ankit.chaturvedi@manprax.com>
2019-09-16 15:36:34 +0530

    1: added check to create period fab

--- OpenEdXMobile/src/main/java/org/edx/mobile/tta/ui/programs/schedule/view_model/ScheduleViewModel.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/tta/ui/programs/schedule/view_model/ScheduleViewModel.java
@@ -65,6 +65,7 @@ public class ScheduleViewModel extends BaseViewModel {
 
     public ObservableBoolean filtersVisible = new ObservableBoolean();
     public ObservableBoolean emptyVisible = new ObservableBoolean();
+    public ObservableBoolean fabVisible = new ObservableBoolean();
 
     public ObservableInt initialPosition = new ObservableInt();
     public ObservableInt toolTipPosition = new ObservableInt();
@@ -118,6 +119,11 @@ public class ScheduleViewModel extends BaseViewModel {
                     ActivityUtil.gotoPage(mActivity, PeriodUnitsActivity.class, parameters1);
             }
         });
+
+
+        if (mDataManager.getLoginPrefs().getRole().equals("Student")){
+            fabVisible.set(false);
+        }else fabVisible.set(true);
 
         mActivity.showLoading();
         getFilters();
