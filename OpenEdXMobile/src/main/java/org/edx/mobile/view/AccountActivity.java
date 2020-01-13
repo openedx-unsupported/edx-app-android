df083e1d575e756e50f8649c83ee218972cedbcf
Farhan Khan <mfarhan133@gmail.com>
2020-01-13 16:11:03 +0500

    Implement deep linking for settings screen
    - LEARNER-7536

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/AccountActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/AccountActivity.java
@@ -2,13 +2,17 @@ package org.edx.mobile.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import org.edx.mobile.base.BaseSingleFragmentActivity;
+import org.edx.mobile.deeplink.ScreenDef;
 import org.edx.mobile.logger.Logger;
 import org.edx.mobile.util.Config;
 
 import javax.inject.Inject;
+
+import static org.edx.mobile.view.Router.EXTRA_SCREEN_NAME;
 
 public class AccountActivity extends BaseSingleFragmentActivity {
     protected Logger logger = new Logger(getClass().getSimpleName());
@@ -16,12 +20,15 @@ public class AccountActivity extends BaseSingleFragmentActivity {
     @Inject
     private Config config;
 
-    public static Intent newIntent(Context activity) {
-        return new Intent(activity, AccountActivity.class);
+    public static Intent newIntent(Context activity, @Nullable @ScreenDef String screenName) {
+        final Intent intent = new Intent(activity, AccountActivity.class);
+        intent.putExtra(EXTRA_SCREEN_NAME, screenName);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        return intent;
     }
 
     @Override
     public Fragment getFirstFragment() {
-        return new AccountFragment();
+        return AccountFragment.newInstance(getIntent().getExtras());
     }
 }
