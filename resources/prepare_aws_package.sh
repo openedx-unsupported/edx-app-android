90aab75bd0fb73e119477db2e58d6e43c2cefb84
Naveed <nsaleem@edx.org>
2020-04-23 20:33:09 +0500

    check paython execution

--- resources/prepare_aws_package.sh
+++ resources/prepare_aws_package.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-# cd $TEST_PROJECT_REPO_NAME
+cd $TEST_PROJECT_REPO_NAME
 
 # prepare wheel house 
-pip wheel --wheel-dir wheelhouse -r edx-app-test/requirements.txt
-sleep 60
+pip wheel --wheel-dir wheelhouse -r requirements.txt
 
-# zip pacakge aws device farm 
-zip -r test_bundle.zip edx-app-test/tests/ wheelhouse/ edx-app-test/requirements.txt
-sleep 30
+# zip pacakge for aws device farm 
+zip -r test_bundle.zip tests/ wheelhouse/ requirements.txt
+
+# Move AWS Package at project root
+cp test_bundle.zip ../
+
+# Move AUT(application under test) at project root
+cp "../OpenEdXMobile/build/outputs/apk/prod/debuggable/$AUT_NAME" ../
+
+cd ..
