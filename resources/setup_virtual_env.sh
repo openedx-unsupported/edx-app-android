0d57d82055a305f0768e2aa611123ab981a69ad2
BilalAwan <bilal.awan@arbisoft.com>
2020-08-18 14:19:39 +0500

    Test Jenkins run change

--- resources/setup_virtual_env.sh
+++ resources/setup_virtual_env.sh
@@ -51,4 +51,4 @@ create_virtual_environment
 switch_to_virtual_env
 install_requirements
 print_message "calling AWS Test run"
-python -v ./resources/trigger_aws_test_run.py $AUT_NAME
+python ./resources/trigger_aws_test_run.py $AUT_NAME
