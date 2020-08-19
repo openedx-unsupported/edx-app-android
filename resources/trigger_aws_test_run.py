de06e55c1bbd312d732a93def5cba2198a3211cb
BilalAwan <bilal.awan@arbisoft.com>
2020-08-19 16:13:53 +0500

    Test jenkins error message

--- resources/trigger_aws_test_run.py
+++ resources/trigger_aws_test_run.py
@@ -247,7 +247,7 @@ def get_test_run(run_arn):
         get_run_details = device_farm.get_run(arn=run_arn)
         run_status = get_run_details['run']['status']
         run_result = get_run_details['run']['result']
-        run_message = get_run_details['run']['message']
+        run_message = get_run_details['message']
         if run_status in RUN_COMPLETED_STATUS:
             print('Run is {} with {} result {} message'.format(run_status, run_result, run_message))
             break
