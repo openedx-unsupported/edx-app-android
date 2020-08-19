73b9e4ebf0d4466d519301ac76f43cf5d1ff5509
BilalAwan <bilal.awan@arbisoft.com>
2020-08-19 16:47:45 +0500

    Test jenkins error message

--- resources/trigger_aws_test_run.py
+++ resources/trigger_aws_test_run.py
@@ -215,7 +215,7 @@ def schedule_run(project_arn, name, device_pool_arn, app_arn,
         test={'type': RUN_TYPE,
               'testPackageArn': test_package_arn,
               'testSpecArn': test_specs_arn
-              },
+              }
     )
 
     run_arn = schedule_run_result['run']['arn']
