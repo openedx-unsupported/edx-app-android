57150fa72be16637efbb8378756c07c32b6809dc
BilalAwan <bilal.awan@arbisoft.com>
2021-01-19 19:09:24 +0500

    Test parameters

--- resources/trigger_aws_test_run.py
+++ resources/trigger_aws_test_run.py
@@ -237,12 +237,13 @@ def schedule_run(project_arn, name, device_pool_arn, app_arn,
     run_arn = schedule_run_result['run']['arn']
 
     test_run = device_farm.get_run(arn=run_arn)
-    print('Run Name - {} on {} is started at {} with status - {} & results - {}'.format(
+    print('Run Name - {} on {} is started at {} with status - {} & results - {} & parameters - {}'.format(
         test_run['run']['name'],
         test_run['run']['platform'],
         str(test_run['run']['created']),
         test_run['run']['status'],
-        test_run['run']['result'])
+        test_run['run']['result'],
+        test_run['run']['parameters'])
     )
     return run_arn
 
