5629c096172fc55f2e137abd04e6ee5a23b17033
Hamza Israr <hamza.israr@arbisoft.com>
2021-05-20 03:49:20 +0500

    feat: extend the strings validator to verify plurals
    - LEARNER-8388
      Extended the python script, for downloading and verifying the
      transifex translations, to verify plural tags too.

--- transifex_utils/core/validators/strings_validator.py
+++ transifex_utils/core/validators/strings_validator.py
@@ -6,18 +6,27 @@ import re
 
 def validate(source_file, template):
     log.log_subline_bold(f"Reading source file : '{source_file}'.")
-    source_file_placeholders_dict = get_source_file_placeholders_dict(
-        source_file)
+    source_file_string_placeholders_dict = get_source_file_string_placeholders_dict(
+        source_file
+    )
+    source_file_plurals_placeholders_dict = get_source_file_plurals_placeholders_dict(
+        source_file
+    )
     for language in constants.languages.values():
         translation_file = template.replace('<lang>', language)
         if not os.path.exists(translation_file):
             log.log_warning(f"'{translation_file}' doesn't exist.")
             continue
         log.log_subline(f"Validating '{translation_file}'.")
-        validate_translation(source_file_placeholders_dict, translation_file)
+        validate_string_translation(
+            source_file_string_placeholders_dict, translation_file
+        )
+        validate_plurals_translation(
+            source_file_plurals_placeholders_dict, translation_file
+        )
 
 
-def validate_translation(source_file_placeholders_dict, translation_file):
+def validate_string_translation(source_file_placeholders_dict, translation_file):
     root = ET.parse(translation_file).getroot()
     for str_element in root.findall('string'):
         str_name = str_element.get('name')
@@ -29,7 +38,23 @@ def validate_translation(source_file_placeholders_dict, translation_file):
                                   str_name + "'\n File: " + translation_file)
 
 
-def get_source_file_placeholders_dict(file):
+def validate_plurals_translation(source_file_placeholders_dict, translation_file):
+    root = ET.parse(translation_file).getroot()
+    for plural_element in root.findall('plurals'):
+        plural_name = plural_element.get('name')
+        if plural_name in source_file_placeholders_dict.keys():
+            for plural_item in plural_element.findall('item'):
+                item_quantity = plural_item.get('quantity')
+                item_value = plural_item.text
+                if item_quantity in source_file_placeholders_dict[plural_name].keys():
+                    for placeholder in source_file_placeholders_dict[plural_name][item_quantity]:
+                        if not placeholder in item_value:
+                            log.log_error(placeholder + " doesn't exist in item '" +
+                                        item_quantity + "' of plural '" + plural_name +
+                                        "'\n File: " + translation_file)
+
+
+def get_source_file_string_placeholders_dict(file):
     """Reads the source xml file and return a dictionary having string name as
     key and tuple of existing placeholders as values"""
     dict = {}
@@ -40,6 +65,21 @@ def get_source_file_placeholders_dict(file):
         placeholders = get_placeholders(str_value)
         if len(placeholders) > 0:
             dict[str_name] = placeholders
+    return dict
+
+
+def get_source_file_plurals_placeholders_dict(file):
+    dict = {}
+    root = ET.parse(file).getroot()
+    for plural_element in root.findall('plurals'):
+        plural_name = plural_element.get('name')
+        for plural_item in plural_element.findall('item'):
+            item_quantity = plural_item.get('quantity')
+            item_value = plural_item.text
+            item_placeholder = get_placeholders(item_value)
+            if len(item_placeholder) > 0:
+                dict.setdefault(plural_name, {})
+                dict[plural_name][item_quantity] = item_placeholder
     return dict
 
 
