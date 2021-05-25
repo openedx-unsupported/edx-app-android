a0280bcf42515e2998a37b1f9a06b4c1a654ac63
BilalAwan <bilal.awan@arbisoft.com>
2021-05-25 16:22:54 +0500

    test jenkins run

--- resources/trigger_aws_test_run.py
+++ resources/trigger_aws_test_run.py
@@ -55,7 +55,7 @@ def aws_job():
     print('package_arn')
     package_arn = upload_file(
         project_arn,
-        PACKAGE_UPLOAD_TYPE,
+        CUSTOM_SPECS_UPLOAD_TYPE,
         PACKAGE_NAME
     )
 
