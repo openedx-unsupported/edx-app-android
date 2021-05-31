7f4244b1ea5cc5f2f46ab66c63b4719951b6fb6a
Hamza Israr <hisrar@edx.org>
2021-05-31 13:39:03 +0500

    Address PR comments

--- transifex_utils/core/validators/strings_validator.py
+++ transifex_utils/core/validators/strings_validator.py
@@ -42,46 +42,52 @@ def validate_plurals_translation(source_file_placeholders_dict, translation_file
     root = ET.parse(translation_file).getroot()
     for plural_element in root.findall('plurals'):
         plural_name = plural_element.get('name')
-        if plural_name in source_file_placeholders_dict.keys():
-            for plural_item in plural_element.findall('item'):
-                item_quantity = plural_item.get('quantity')
-                item_value = plural_item.text
-                if item_quantity in source_file_placeholders_dict[plural_name].keys():
-                    for placeholder in source_file_placeholders_dict[plural_name][item_quantity]:
-                        if not placeholder in item_value:
-                            log.log_error(placeholder + " doesn't exist in item '" +
-                                        item_quantity + "' of plural '" + plural_name +
-                                        "'\n File: " + translation_file)
+        if plural_name not in source_file_placeholders_dict.keys():
+            continue
+        for plural_item in plural_element.findall('item'):
+            item_quantity = plural_item.get('quantity')
+            item_value = plural_item.text
+            if item_quantity not in source_file_placeholders_dict[plural_name].keys():
+                continue
+            for placeholder in source_file_placeholders_dict[plural_name][item_quantity]:
+                if not placeholder in item_value:
+                    log.log_error(placeholder + " doesn't exist in item '" +
+                                  item_quantity + "' of plural '" + plural_name +
+                                  "'\n File: " + translation_file)
 
 
 def get_source_file_string_placeholders_dict(file):
     """Reads the source xml file and return a dictionary having string name as
     key and tuple of existing placeholders as values"""
-    dict = {}
+    placeholders_dict = {}
     root = ET.parse(file).getroot()
     for str_element in root.findall('string'):
         str_name = str_element.get('name')
         str_value = ''.join(str_element.itertext())
         placeholders = get_placeholders(str_value)
-        if len(placeholders) > 0:
-            dict[str_name] = placeholders
-    return dict
+        if placeholders:
+            placeholders_dict[str_name] = placeholders
+    return placeholders_dict
 
 
 def get_source_file_plurals_placeholders_dict(file):
-    dict = {}
+    placeholders_dict = {}
     root = ET.parse(file).getroot()
     for plural_element in root.findall('plurals'):
-        plural_name = plural_element.get('name')
-        for plural_item in plural_element.findall('item'):
-            item_quantity = plural_item.get('quantity')
-            item_value = plural_item.text
-            item_placeholder = get_placeholders(item_value)
-            if len(item_placeholder) > 0:
-                dict.setdefault(plural_name, {})
-                dict[plural_name][item_quantity] = item_placeholder
-    return dict
+        placeholders_dict.update(get_plural_items_placeholders(plural_element))
+    return placeholders_dict
 
+
+def get_plural_items_placeholders(plural_element):
+    placeholders_dict = {}
+    name = plural_element.get('name')
+    for item in plural_element.findall('item'):
+        item_quantity = item.get('quantity')
+        item_value = item.text
+        item_placeholder = get_placeholders(item_value)
+        if item_placeholder:
+            placeholders_dict[name] = {item_quantity: item_placeholder}
+    return placeholders_dict
 
 def get_placeholders(str):
     return re.findall("{?[a-zA-Z0-9_]+}", str)
