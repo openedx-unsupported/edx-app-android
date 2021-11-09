4751c0d817d1b2f34d864263998d0b5de98a375c
Hamza Israr <hisrar@edx.org>
2021-11-09 20:39:20 +0500

    feat: Update profile json validator for year of birth
    fix: LEARNER-8620

--- transifex_utils/core/validators/profiles_json_validator.py
+++ transifex_utils/core/validators/profiles_json_validator.py
@@ -24,7 +24,6 @@ def validate_translation_data(source_data, translation_data, translation_file):
     validate_field_1(source_data, translation_data, translation_file)
     validate_field_2(source_data, translation_data, translation_file)
     validate_field_3(source_data, translation_data, translation_file)
-    validate_field_4(source_data, translation_data, translation_file)
 
 
 def validate_field_0(source_data, translation_data, translation_file):
@@ -66,19 +65,15 @@ def validate_field_1(source_data, translation_data, translation_file):
     # type
     validator_util.match_property(i, "type", source_data, translation_data,
                                   translation_file)
-    # ["options"]["range_min"]
+    # data_type
+    validator_util.match_property(i, "data_type", source_data,
+                                  translation_data, translation_file)
+    # ["options"]["reference"]
     validator_util.match_particular_property(i,
                                              source_data[i]["options"]
-                                             ["range_min"],
+                                             ["reference"],
                                              translation_data[i]["options"]
-                                             ["range_min"],
-                                             translation_file)
-    # ["options"]["range_max"]
-    validator_util.match_particular_property(i,
-                                             source_data[i]["options"]
-                                             ["range_max"],
-                                             translation_data[i]["options"]
-                                             ["range_max"],
+                                             ["reference"],
                                              translation_file)
     # ["options"]["allows_none"]
     validator_util.match_particular_property(i,
@@ -120,34 +115,6 @@ def validate_field_2(source_data, translation_data, translation_file):
 def validate_field_3(source_data, translation_data, translation_file):
     # index
     i = 3
-    # name
-    validator_util.match_property(i, "name", source_data, translation_data,
-                                  translation_file)
-    # type
-    validator_util.match_property(i, "type", source_data, translation_data,
-                                  translation_file)
-    # data_type
-    validator_util.match_property(i, "data_type", source_data,
-                                  translation_data, translation_file)
-    # ["options"]["reference"]
-    validator_util.match_particular_property(i,
-                                             source_data[i]["options"]
-                                             ["reference"],
-                                             translation_data[i]["options"]
-                                             ["reference"],
-                                             translation_file)
-    # ["options"]["allows_none"]
-    validator_util.match_particular_property(i,
-                                             source_data[i]["options"]
-                                             ["allows_none"],
-                                             translation_data[i]["options"]
-                                             ["allows_none"],
-                                             translation_file)
-
-
-def validate_field_4(source_data, translation_data, translation_file):
-    # index
-    i = 4
     # name
     validator_util.match_property(i, "name", source_data, translation_data,
                                   translation_file)
