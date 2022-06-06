82bb8022c3901f6a5a6b8a10e7d39792e2b7f971
Farhan Arshad <farhan.arshad@arbisoft.com>
2022-06-06 15:30:14 +0500

    CourseCardUtilsTest move in unit test cases

--- OpenEdXMobile/src/test/java/org/edx/mobile/util/CourseCardUtilsTest.java
+++ OpenEdXMobile/src/test/java/org/edx/mobile/util/CourseCardUtilsTest.java
@@ -1,20 +1,19 @@
-package org.edx.mobile.test;
+package org.edx.mobile.util;
+
+import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.edx.mobile.base.MainApplication;
 import org.edx.mobile.model.api.StartType;
-import org.edx.mobile.util.DateUtil;
 import org.edx.mobile.util.images.CourseCardUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Date;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class CourseCardUtilsTest {
@@ -24,7 +23,7 @@ public class CourseCardUtilsTest {
 
     @Before
     public void setup() {
-        context = MainApplication.instance();
+        context = ApplicationProvider.getApplicationContext();
         today = DateUtil.convertToDate("2018-12-01T00:00:00+05:00"); // Today: 1 December 2018
         end = "2019-12-01T00:00:00+05:00";
     }
