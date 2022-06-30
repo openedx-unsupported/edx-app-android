d9786be4d047164d8f97932d6d78e00f7da3cd45
Farhan Arshad <farhan.arshad@arbisoft.com>
2022-06-30 12:20:58 +0500

    feat: rename `Discover` to `Discovery` for new discovery experience
    - Rename `Discover` to `Discovery` for new discovery experience and remove translations.
    
    Fixes: LEARNER-8909

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/DiscoveryActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/DiscoveryActivity.java
@@ -41,7 +41,7 @@ public class DiscoveryActivity extends BaseSingleFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle(R.string.label_discover);
+        setTitle(R.string.label_discovery);
         environment.getAnalyticsRegistry().trackScreenView(Analytics.Screens.FIND_COURSES);
     }
 
