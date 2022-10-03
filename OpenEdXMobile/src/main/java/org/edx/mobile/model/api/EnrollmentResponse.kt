d427efb8c0edc461624c0ec3f5d371cbe4c3b3e9
Hamza Israr <hisrar@edx.org>
2022-10-03 15:02:04 +0500

    refactor: Add doc string and optimize code
    Fixes: LEARNER-9072

--- OpenEdXMobile/src/main/java/org/edx/mobile/model/api/EnrollmentResponse.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/model/api/EnrollmentResponse.kt
@@ -6,7 +6,6 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
-import com.google.gson.JsonParser
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 import org.edx.mobile.logger.Logger
@@ -40,12 +39,18 @@ data class EnrollmentResponse(
                         Gson().fromJson((json as JsonArray), listType)
                     )
                 } else {
+                    /**
+                     * To remove dependency on the backend, all the data related to Remote Config
+                     * will be received under the `configs` key. The `config` is the key under
+                     * 'configs` which defines the data that is related to the configuration of the
+                     * app.
+                     */
                     val config = (json as JsonObject)
                         .getAsJsonObject("configs")
                         .getAsJsonPrimitive("config")
 
                     val appConfig = Gson().fromJson(
-                        JsonParser.parseString(config.asString),
+                        config.asString,
                         AppConfig::class.java
                     )
                     val enrolledCourses = Gson().fromJson<List<EnrolledCoursesResponse>>(
