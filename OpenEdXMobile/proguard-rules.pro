ce20b4f0418581b75527cd4a9402759d5d280222
Omer Habib <omer.habib26@gmail.com>
2022-10-17 21:13:40 +0500

    fix: Downgrade microsoft library to make it compatible with prod release - Add rules for microsoft library - Add rules for http model class - Add rules for registration model classes LEARNER-9087

--- OpenEdXMobile/proguard-rules.pro
+++ OpenEdXMobile/proguard-rules.pro
@@ -11,6 +11,8 @@
 
 # Keep Rule for Model classes
 -keep class org.edx.mobile.model.** {*;}
+-keep class org.edx.mobile.http.model.** {*;}
+-keep class org.edx.mobile.module.registration.model.** {*;}
 
 # Keep Rule for Webviews with JS
 -keepattributes JavascriptInterface
@@ -18,8 +20,14 @@
     @android.webkit.JavascriptInterface <methods>;
 }
 
+# Keep Rules for Microsoft Auth
+-keepclassmembers class com.microsoft.identity.** {
+    <fields>;
+    <methods>;
+}
+
 # Keep Rule to preserve the line number information for debugging stack traces
--keepattributes SourceFile,LineNumberTable
+-keepattributes SourceFile, LineNumberTable
 
 # Keep Rule to hide the original source file name on stack trace
 -renamesourcefileattribute SourceFile
--- OpenEdXMobile/src/main/java/org/edx/mobile/social/microsoft/MicrosoftAuth.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/social/microsoft/MicrosoftAuth.kt
@@ -3,66 +3,50 @@ package org.edx.mobile.social.microsoft
 import android.app.Activity
 import android.content.Intent
 import com.microsoft.identity.client.AuthenticationCallback
-import com.microsoft.identity.client.IAccount
 import com.microsoft.identity.client.IAuthenticationResult
-import com.microsoft.identity.client.IMultipleAccountPublicClientApplication
-import com.microsoft.identity.client.IPublicClientApplication
-import com.microsoft.identity.client.IPublicClientApplication.IMultipleAccountApplicationCreatedListener
 import com.microsoft.identity.client.PublicClientApplication
 import com.microsoft.identity.client.exception.MsalException
 import org.edx.mobile.R
 import org.edx.mobile.social.ISocialImpl
 
 class MicrosoftAuth(activity: Activity?) : ISocialImpl(activity) {
-    private var microsoftClient: IMultipleAccountPublicClientApplication? = null
-
+    private var microsoftClient: PublicClientApplication? = null
     override fun login() {
-        PublicClientApplication.createMultipleAccountPublicClientApplication(this.activity,
-            R.raw.auth_config,
-            object : IMultipleAccountApplicationCreatedListener {
-                override fun onCreated(application: IMultipleAccountPublicClientApplication) {
-                    microsoftClient = application
-                    microsoftClient?.acquireToken(activity, SCOPES,
-                        object : AuthenticationCallback {
-                            override fun onSuccess(authenticationResult: IAuthenticationResult) {
-                                callback?.onLogin(authenticationResult.accessToken)
-                                logger.debug("Microsoft Logged in successfully.")
-                            }
+        microsoftClient = PublicClientApplication(this.activity, R.raw.auth_config)
+        microsoftClient?.acquireToken(activity, SCOPES, object : AuthenticationCallback {
+            override fun onSuccess(authenticationResult: IAuthenticationResult) {
+                callback?.onLogin(authenticationResult.accessToken)
+                logger.debug("Microsoft Logged in successfully.")
+            }
 
-                            override fun onError(exception: MsalException) {
-                                callback?.onError(exception)
-                                logger.error(exception, true)
-                            }
+            override fun onError(exception: MsalException) {
+                callback?.onError(exception)
+                logger.error(exception, true)
+            }
 
-                            override fun onCancel() {
-                                callback?.onCancel()
-                                logger.debug("Microsoft Log in canceled.")
-                            }
-                        })
-                }
-
-                override fun onError(exception: MsalException) {
-                    logger.error(exception, true)
-                    callback?.onError(exception)
-                }
-            })
+            override fun onCancel() {
+                callback?.onCancel()
+                logger.debug("Microsoft Log in canceled.")
+            }
+        })
     }
 
     override fun logout() {
-        microsoftClient?.getAccounts(object : IPublicClientApplication.LoadAccountsCallback {
-            override fun onTaskCompleted(result: MutableList<IAccount>?) {
-                result?.forEach { microsoftClient?.removeAccount(it) }
+        microsoftClient?.getAccounts { accounts ->
+            if (accounts.isNotEmpty()) {
+                for (account in accounts) {
+                    // Pass empty callback because sometime throw `NullPointerException`
+                    // due to null callback
+                    microsoftClient?.removeAccount(
+                        account) { }
+                }
             }
-
-            override fun onError(exception: MsalException?) {
-                logger.error(exception, true)
-            }
-        })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {}
 
     companion object {
-        private val SCOPES = arrayOf("User.Read")
+        private val SCOPES = arrayOf("https://graph.microsoft.com/User.Read")
     }
 }
