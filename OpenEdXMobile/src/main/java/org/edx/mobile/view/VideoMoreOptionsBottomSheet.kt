3794b77fa73e2ca4b90d98ff54af7b656bb7383c
Hamza Israr <hisrar@edx.org>
2022-12-08 15:24:49 +0500

    refactor: Add doc string and remove landscape behaviour

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/VideoMoreOptionsBottomSheet.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/VideoMoreOptionsBottomSheet.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.edx.mobile.databinding.BottomSheetVideoMoreOptionsBinding
@@ -25,16 +23,6 @@ class VideoMoreOptionsBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = BottomSheetVideoMoreOptionsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (dialog as BottomSheetDialog).behavior.apply {
-            // To expand the modal in landscape mode
-            maxWidth = ViewGroup.LayoutParams.MATCH_PARENT
-            state = BottomSheetBehavior.STATE_EXPANDED
-        }
 
         binding.deleteOption.setOnClickListener {
             videoViewModel.deleteVideosAtPosition(
@@ -42,6 +30,8 @@ class VideoMoreOptionsBottomSheet : BottomSheetDialogFragment() {
             )
             dismiss()
         }
+
+        return binding.root
     }
 
     override fun onDismiss(dialog: DialogInterface) {
--- OpenEdXMobile/src/main/java/org/edx/mobile/viewModel/VideoViewModel.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/viewModel/VideoViewModel.kt
@@ -6,13 +6,18 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+/**
+ * The [VideoViewModel] is shared between Parent & Child Fragment so they can only communicate with
+ * each other. In this way, the fragments do not need to know about each other, and the activity
+ * does not need to do anything to facilitate the communication.
+ */
 @HiltViewModel
 class VideoViewModel @Inject constructor() : ViewModel() {
 
-    private val _clearChoices = MutableLiveData(false)
+    private val _clearChoices = MutableLiveData<Boolean>()
     val clearChoices: LiveData<Boolean> = _clearChoices
 
-    private val _selectedVideosPosition = MutableLiveData(-1)
+    private val _selectedVideosPosition = MutableLiveData<Int>()
     val selectedVideosPosition: LiveData<Int> = _selectedVideosPosition
 
     fun deleteVideosAtPosition(position: Int) {
