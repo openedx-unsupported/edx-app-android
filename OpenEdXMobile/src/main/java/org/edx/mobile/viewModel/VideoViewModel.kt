72213075600ee992f56ecdb87fe9043c4e6ca8a5
Hamza Israr <71447999+HamzaIsrar12@users.noreply.github.com>
2022-12-12 17:45:36 +0500

    feat: Delete videos through Modal bottom sheet (#1728)
    feat: Delete videos through Modal bottom sheet
    
    We will show the Modal bottom sheet on long press on the course
    list items (downloaded videos) instead of the Title bar to delete
    videos.
    
    Fixes: LEARNER-9137

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/viewModel/VideoViewModel.kt
@@ -0,0 +1,32 @@
+package org.edx.mobile.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.edx.mobile.util.observer.Event
+import org.edx.mobile.util.observer.postEvent
+import javax.inject.Inject
+
+/**
+ * The [VideoViewModel] is shared between Parent & Child Fragment so they can only communicate with
+ * each other. In this way, the fragments do not need to know about each other, and the activity
+ * does not need to do anything to facilitate the communication.
+ */
+@HiltViewModel
+class VideoViewModel @Inject constructor() : ViewModel() {
+
+    private val _clearChoices = MutableLiveData<Event<Boolean>>()
+    val clearChoices: LiveData<Event<Boolean>> = _clearChoices
+
+    private val _selectedVideosPosition = MutableLiveData<Event<Int>>()
+    val selectedVideosPosition: LiveData<Event<Int>> = _selectedVideosPosition
+
+    fun deleteVideosAtPosition(position: Int) {
+        _selectedVideosPosition.postEvent(position)
+    }
+
+    fun clearChoices() {
+        _clearChoices.postEvent(true)
+    }
+}
