7e7e6d81bfe2624a0b9cdc89dd6d81a5892e68f2
Hamza Israr <71447999+HamzaIsrar12@users.noreply.github.com>
2023-02-13 16:58:33 +0500

    fix: NPE on Posts in Discussion tab (#1742)
    fix: NPE on Posts in the Discussion tab
    
    - The AttributeSet of the EdxDiscussionBody was not applicable for the
      internal views and was causing a NullPointerException (NPE) on some
      devices.
    - The addView functions simply add another view in the root FrameLayout
      which was casing overlapping issue when items were recycled in Recycler
      View.
    
    Fixes: LEARNER-9224

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/EdxDiscussionBody.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/EdxDiscussionBody.kt
@@ -2,16 +2,35 @@ package org.edx.mobile.view.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.core.widget.TextViewCompat
+import org.edx.mobile.R
 import org.edx.mobile.extenstion.renderHtml
 import java.util.regex.Pattern
 
 class EdxDiscussionBody @JvmOverloads constructor(
     context: Context,
-    private val attrs: AttributeSet? = null,
-    private val defStyleAttr: Int = 0
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val bodyTextAppearance: Int
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs, R.styleable.EdxDiscussionBody, 0, 0
+        ).apply {
+            try {
+                bodyTextAppearance = getResourceId(
+                    R.styleable.EdxDiscussionBody_bodyTextAppearance, 0
+                )
+            } finally {
+                recycle()
+            }
+        }
+    }
 
     /**
      * Method to render the [body] based on HTML paragraph tag
@@ -19,12 +38,15 @@ class EdxDiscussionBody @JvmOverloads constructor(
      */
     fun setBody(body: String?) {
         body?.let {
+            this.removeAllViews()
             if (isPlainHtml(body)) {
-                this.addView(TextView(context, attrs, defStyleAttr).also {
+                this.addView(TextView(context).also {
+                    TextViewCompat.setTextAppearance(it, bodyTextAppearance)
                     it.renderHtml(body)
                 })
             } else {
-                this.addView(EdxWebView(context, attrs).also {
+                this.addView(EdxWebView(context, null).also {
+                    it.layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
                     it.loadDataWithBaseURL(null, body, "text/html", "utf-8", null)
                 })
             }
