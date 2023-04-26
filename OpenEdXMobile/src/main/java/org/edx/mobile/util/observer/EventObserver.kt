1f0c6db82ca74371a38c97fa6c77fadf4467927a
Farhan Arshad <farhan.arshad@arbisoft.com>
2023-04-26 14:01:00 +0500

    build: bump the TARGET_SDK_VERSION to api level 33.
    - bump the `KOTLIN_VERSION` to `1.8.0`
    - bump the `GRADLE_PLUGIN_VERSION` to `7.0.4`
    - bump the `hilt_version` to `2.44`
    - bump the `androidXLifeCycle` to `2.6.1`
    - bump the `hilt_version` to `1.10.0`

--- OpenEdXMobile/src/main/java/org/edx/mobile/util/observer/EventObserver.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/util/observer/EventObserver.kt
@@ -10,8 +10,9 @@ import androidx.lifecycle.Observer
  * Inspiration: https://gist.github.com/JoseAlcerreca/e0bba240d9b3cffa258777f12e5c0ae9
  */
 class EventObserver<T>(private val consumer: (T) -> Unit) : Observer<Event<T>> {
-    override fun onChanged(event: Event<T>?) {
-        consumer(event?.getContentIfNotConsumed() ?: return)
+
+    override fun onChanged(value: Event<T>) {
+        consumer(value.getContentIfNotConsumed() ?: return)
     }
 }
 
