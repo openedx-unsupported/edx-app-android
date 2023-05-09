118f6d1773313fd5e2147b5ed4929fbece3fa465
Farhan Arshad <43750646+farhan-arshad-dev@users.noreply.github.com>
2023-05-09 16:13:41 +0500

    refactor: Implement Youtube 3rd party library as official Youtube SDK is no longer supported (#1778)
    build: bump the TARGET_SDK_VERSION to API level 33.
    
    - bump the `KOTLIN_VERSION` to `1.8.0`
    - bump the `GRADLE_PLUGIN_VERSION` to `7.0.4`
    - bump the `hilt_version` to `2.44`
    - bump the `androidXLifeCycle` to `2.6.1`
    - bump the `hilt_version` to `1.10.0`
    
    * refactor: replace the official Youtube library with 3rd party library
    
    - Now that the official API from Google is deprecated, so replace the official Youtube library 3rd party.
    - bump `targetSdk` to API level 33, and support libraries
    
    Ref:
    - https://developers.google.com/youtube/android/player/
    - https://github.com/PierfrancescoSoffritti/android-youtube-player
    
    fixes: LEARNER-8794

--- OpenEdXMobile/src/main/java/org/edx/mobile/util/Config.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/util/Config.java
@@ -476,15 +476,8 @@ public class Config {
         @SerializedName("ENABLED")
         private boolean enabled;
 
-        @SerializedName("API_KEY")
-        private String apiKey;
-
         public boolean isYoutubePlayerEnabled() {
             return enabled;
-        }
-
-        public String getApiKey() {
-            return apiKey;
         }
     }
 
