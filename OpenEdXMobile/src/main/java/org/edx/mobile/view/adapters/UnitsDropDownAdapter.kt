395c989d45797d3e3bb632a08af0e4d011d5d210
Farhan Arshad <farhan.arshad@arbisoft.com>
2023-07-29 00:06:28 +0500

    feat: add table of contents from subsection in course unit
    - Tapping the unit title brings up the Table of Content from the subsection in a dropdown.
    - Checkmarks to be shown on with the completed units.
    - Lock icon to be shown for units with locked content
    - Tapping a unit from the dropdown closes the dropdown and brings the user to the tapped unit
    - Hide the drop-down if there is only one unit(no other unit to show) in the sub-section.
    
    fixes: LEARNER-9300

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/adapters/UnitsDropDownAdapter.kt
@@ -0,0 +1,70 @@
+package org.edx.mobile.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import org.edx.mobile.R
+import org.edx.mobile.databinding.LayoutUnitDropDownItemBinding
+import org.edx.mobile.extenstion.setVisibility
+import org.edx.mobile.model.api.AuthorizationDenialReason
+import org.edx.mobile.model.api.EnrolledCoursesResponse
+import org.edx.mobile.model.course.CourseComponent
+import org.edx.mobile.model.course.IBlock
+
+class UnitsDropDownAdapter(
+    private val courseData: EnrolledCoursesResponse?,
+    private val units: MutableList<IBlock>,
+    private val listener: OnItemSelect?
+) : RecyclerView.Adapter<UnitsDropDownAdapter.UnitDropDownViewHolder>() {
+
+    private var selectedItemPosition: Int = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnitDropDownViewHolder {
+        val binding = LayoutUnitDropDownItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return UnitDropDownViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UnitDropDownViewHolder, position: Int) {
+        val unit = units[position]
+        holder.binding.tvUnitTitle.text = unit.displayName
+        holder.binding.ivUnitStatus.setVisibility(true)
+        if (unit.isCompleted) {
+            holder.binding.ivUnitStatus.setImageResource(R.drawable.ic_green_check)
+        } else if (courseData?.isUpgradeable == true &&
+            unit is CourseComponent &&
+            unit.authorizationDenialReason == AuthorizationDenialReason.FEATURE_BASED_ENROLLMENTS
+        ) {
+            holder.binding.ivUnitStatus.setImageResource(R.drawable.ic_white_lock_secondary_base)
+        } else {
+            holder.binding.ivUnitStatus.setVisibility(false)
+        }
+        holder.binding.divider.setVisibility(position < itemCount - 1)
+        holder.binding.rlContent.background =
+            ContextCompat.getDrawable(holder.binding.rlContent.context, R.drawable.view_selector)
+        holder.binding.rlContent.isSelected = selectedItemPosition == position
+        holder.binding.rlContent.setOnClickListener {
+            listener?.onUnitSelect(unit)
+        }
+    }
+
+    fun setSelection(position: Int) {
+        selectedItemPosition = position
+        notifyItemChanged(selectedItemPosition)
+    }
+
+    override fun getItemCount(): Int {
+        return units.size
+    }
+
+    class UnitDropDownViewHolder(val binding: LayoutUnitDropDownItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    interface OnItemSelect {
+        fun onUnitSelect(unit: IBlock)
+    }
+}
--- OpenEdXMobile/src/main/java/org/edx/mobile/view/app_nav/NewCourseUnitNavigationActivity.kt
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/app_nav/NewCourseUnitNavigationActivity.kt
@@ -1,22 +1,29 @@
 package org.edx.mobile.view.app_nav
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import dagger.hilt.android.AndroidEntryPoint
 import org.edx.mobile.R
 import org.edx.mobile.base.BaseFragmentActivity
 import org.edx.mobile.databinding.ActivityCourseUnitNavigationNewBinding
+import org.edx.mobile.databinding.LayoutUnitsDropDownBinding
 import org.edx.mobile.extenstion.CollapsingToolbarStatListener
 import org.edx.mobile.extenstion.setTextWithIcon
 import org.edx.mobile.extenstion.setTitleStateListener
 import org.edx.mobile.model.api.CourseUpgradeResponse
 import org.edx.mobile.model.api.EnrolledCoursesResponse
 import org.edx.mobile.model.course.CourseComponent
+import org.edx.mobile.model.course.IBlock
 import org.edx.mobile.services.CourseManager
 import org.edx.mobile.util.AppConstants
+import org.edx.mobile.util.UiUtils
 import org.edx.mobile.util.ViewAnimationUtil
 import org.edx.mobile.util.images.ImageUtils
 import org.edx.mobile.view.Router
+import org.edx.mobile.view.adapters.UnitsDropDownAdapter
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -52,23 +59,53 @@ class NewCourseUnitNavigationActivity : BaseFragmentActivity() {
 
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        restore(savedInstanceState)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun onLoadData() {
+        component =
+            courseData?.courseId?.let { courseId ->
+                courseComponentId?.let { componentId ->
+                    courseManager.getComponentById(courseId, componentId)
+                }
+            }
+    }
+
+    private fun restore(bundle: Bundle?) {
+        bundle?.let {
+            courseData =
+                bundle.getSerializable(Router.EXTRA_COURSE_DATA) as EnrolledCoursesResponse?
+            courseUpgradeData = bundle.getParcelable(Router.EXTRA_COURSE_UPGRADE_DATA)
+            courseComponentId = bundle.getString(Router.EXTRA_COURSE_COMPONENT_ID)
+        }
+    }
+
     private fun setupToolbar() {
-        // Sub section title
-        binding.courseSubSectionTitle.text = component?.displayName
-        val dropdownIcon = ImageUtils.rotateVectorDrawable(this, R.drawable.ic_play_arrow, 90f)
-        val firstIncompleteUnit = component?.firstIncompleteComponent
-        val unitTitle = firstIncompleteUnit?.displayName + "  " + AppConstants.ICON_PLACEHOLDER
-        binding.collapsedToolbarTitle.text = firstIncompleteUnit?.displayName
-        ViewAnimationUtil.startAlphaAnimation(
-            binding.collapsedToolbarTitle,
-            View.INVISIBLE
-        )
-        // first incomplete unit title
-        binding.courseUnitTitle.setTextWithIcon(
-            unitTitle,
-            dropdownIcon,
-            AppConstants.ICON_PLACEHOLDER
-        )
+        component?.let { subSection ->
+            // Sub section title
+            binding.courseSubSectionTitle.text = subSection.displayName
+
+            val hasMultipleChildren =
+                subSection.children.isNullOrEmpty().not() && subSection.children.size > 1
+            subSection.firstIncompleteComponent?.let {
+                setUnitTitle(it, hasMultipleChildren)
+                if (hasMultipleChildren) {
+                    setupUnitsDropDown(subSection.children, it)
+                }
+            }
+
+            ViewAnimationUtil.startAlphaAnimation(
+                binding.collapsedToolbarTitle,
+                View.INVISIBLE
+            )
+            setupToolbarListeners()
+        }
+    }
+
+    private fun setupToolbarListeners() {
         binding.appbar.setTitleStateListener(
             binding.collapsingToolbarLayout,
             object : CollapsingToolbarStatListener {
@@ -98,27 +135,48 @@ class NewCourseUnitNavigationActivity : BaseFragmentActivity() {
         binding.ivCollapsedBack.setOnClickListener { finish() }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        restore(savedInstanceState)
+    private fun setUnitTitle(firstIncompleteUnit: CourseComponent, hasMoreThenOneChild: Boolean) {
+        val dropdownIcon = ImageUtils.rotateVectorDrawable(this, R.drawable.ic_play_arrow, 90f)
+        var unitTitle = firstIncompleteUnit.displayName
+        if (hasMoreThenOneChild) {
+            unitTitle += "  " + AppConstants.ICON_PLACEHOLDER
+        }
+        binding.collapsedToolbarTitle.text = firstIncompleteUnit.displayName
+        // first incomplete unit title
+        binding.courseUnitTitle.setTextWithIcon(
+            unitTitle,
+            dropdownIcon,
+            AppConstants.ICON_PLACEHOLDER
+        )
     }
 
-    @Suppress("DEPRECATION")
-    private fun onLoadData() {
-        component =
-            courseData?.courseId?.let { courseId ->
-                courseComponentId?.let { componentId ->
-                    courseManager.getComponentById(courseId, componentId)
-                }
-            }
-    }
+    private fun setupUnitsDropDown(units: MutableList<IBlock>, selectedUnits: CourseComponent) {
+        binding.courseUnitTitle.setOnClickListener {
+            val popupViewBinding =
+                LayoutUnitsDropDownBinding.inflate(LayoutInflater.from(it.context), null, false)
+            val popupWindow = PopupWindow()
+            popupWindow.contentView = popupViewBinding.root
+            popupWindow.width = binding.flUnitsDropDown.width
+            popupWindow.height = ViewGroup.LayoutParams.WRAP_CONTENT
+            popupWindow.isOutsideTouchable = true
+            popupWindow.setBackgroundDrawable(getDrawable(R.drawable.white_roundedbg))
 
-    private fun restore(bundle: Bundle?) {
-        bundle?.let {
-            courseData =
-                bundle.getSerializable(Router.EXTRA_COURSE_DATA) as EnrolledCoursesResponse?
-            courseUpgradeData = bundle.getParcelable(Router.EXTRA_COURSE_UPGRADE_DATA)
-            courseComponentId = bundle.getString(Router.EXTRA_COURSE_COMPONENT_ID)
+            val adapter =
+                UnitsDropDownAdapter(courseData, units, object : UnitsDropDownAdapter.OnItemSelect {
+                    override fun onUnitSelect(unit: IBlock) {
+                        setUnitTitle(unit as CourseComponent, true)
+                        popupWindow.dismiss()
+                    }
+                })
+            popupViewBinding.rvUnits.adapter = adapter
+
+            popupWindow.elevation =
+                UiUtils.dpToPx(this, resources.getDimension(R.dimen.edx_quarter_margin))
+            // Show the popup below the anchor view with the specified offsets in pixels
+            popupWindow.showAsDropDown(
+                binding.flUnitsDropDown, 0,
+                UiUtils.dpToPx(this, resources.getDimension(R.dimen.edx_x_quarter_margin)).toInt()
+            )
         }
     }
 }
